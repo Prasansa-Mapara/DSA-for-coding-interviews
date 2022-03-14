614f749c239f782a3117bb5f6ca6944dfc6fbedb
Prasansa Mapara <94972192+ced7diggory@users.noreply.github.com>
2022-03-14 22:57:20 +0530

    Time: 42 ms (28.95%), Space: 10.8 MB (24.51%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int l=0, r=0, sol=0, n=s.size();
-        unordered_map<char, int> hash;
+        int l=0, r=0, n=s.size(), sol=0;
+        unordered_set<char> hash;
         while(r<n){
-            while(hash[s[r]]){
-                hash[s[l]]--;
+            if(hash.find(s[r])!=hash.end()){ //char exists
+                hash.erase(s[l]);
                 l++;
             }
-            hash[s[r]]++;
-            sol=max(sol, r-l+1);
-            r++;
+            else{ //char doesn't exist
+                hash.insert(s[r]);
+                sol=max(r-l+1, sol);
+                r++;
+            }
         }
         return sol;
     }
