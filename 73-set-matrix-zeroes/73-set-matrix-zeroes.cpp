ea9e4f848116ba61d4ca8fd1a2d262538d0e379d
Prasansa Mapara <94972192+ced7diggory@users.noreply.github.com>
2022-03-20 23:24:48 +0530

    Time: 26 ms (28.23%), Space: 13.1 MB (95.61%) - LeetHub

---
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& mat) {
+        int r=mat.size(), c=mat[0].size();
+        bool rr=1, cc=1;
+        for(int j=0; j<c; j++){
+            if(mat[0][j]==0){
+                rr=0;
+                break;
+            }
+        }
+        for(int i=0; i<r; i++){
+            if(mat[i][0]==0){
+                cc=0;
+                break;
+            }
+        }
+        for(int i=1; i<r; i++){
+            for(int j=1; j<c; j++){
+                if(mat[i][j]==0){
+                    mat[i][0]=0;
+                    mat[0][j]=0;
+                }
+            }
+        }
+        for(int i=1; i<r; i++){
+            if(mat[i][0]==0){
+                for(int j=0; j<c; j++){
+                    mat[i][j]=0;
+                }
+            }
+        }
+        for(int j=1; j<c; j++){
+            if(mat[0][j]==0){
+                for(int i=0; i<r; i++){
+                    mat[i][j]=0;
+                }
+            }
+        }
+        if(rr==0){
+            for(int j=0; j<c; j++) mat[0][j]=0;
+        }
+        if(cc==0){
+            for(int i=0; i<r; i++) mat[i][0]=0;
+        }
+    }
+};
