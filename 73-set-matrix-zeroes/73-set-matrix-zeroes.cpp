0821b916af89f700381f11532e1d6ce55494d4c5
Prasansa Mapara <94972192+ced7diggory@users.noreply.github.com>
2022-03-20 23:28:38 +0530

    Time: 15 ms (78.27%), Space: 13.3 MB (54.58%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -24,15 +24,8 @@ public:
             }
         }
         for(int i=1; i<r; i++){
-            if(mat[i][0]==0){
-                for(int j=0; j<c; j++){
-                    mat[i][j]=0;
-                }
-            }
-        }
-        for(int j=1; j<c; j++){
-            if(mat[0][j]==0){
-                for(int i=0; i<r; i++){
+            for(int j=1; j<c; j++){
+                if(mat[i][0]==0 || mat[0][j]==0){
                     mat[i][j]=0;
                 }
             }
