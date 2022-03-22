91e93b28a421c49b99b689e2f034f28fb26272eb
Prasansa Mapara <94972192+ced7diggory@users.noreply.github.com>
2022-03-22 16:08:13 +0530

    Time: 105 ms (89.30%), Space: 61.3 MB (61.18%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -1,13 +1,23 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int n=nums.size();
-        for(int i=1; i<n; i++){
-            if(nums[i]==nums[i-1]){
-                return nums[i];
-            }
+
+        // Find the intersection point of the two runners.
+        int tortoise = nums[0];
+        int hare = nums[0];
+
+        do {
+            tortoise = nums[tortoise];
+            hare = nums[nums[hare]];
+        } while (tortoise != hare);
+
+        // Find the "entrance" to the cycle.
+        tortoise = nums[0];
+        while (tortoise != hare) {
+            tortoise = nums[tortoise];
+            hare = nums[hare];
         }
-        return -1;
+
+        return hare;
     }
 };
