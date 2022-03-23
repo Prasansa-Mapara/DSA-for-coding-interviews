584aa7ffe4832614f200e93bd765de9792c4b433
Prasansa Mapara <94972192+ced7diggory@users.noreply.github.com>
2022-03-23 16:27:05 +0530

    Time: 199 ms (34.08%), Space: 61.1 MB (91.54%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -3,12 +3,16 @@ public:
     int findDuplicate(vector<int>& nums) {
         int n=nums.size();
         int slow=nums[0], fast=nums[nums[0]];
-        while(slow!=fast){
+        while(slow!=fast){ //detecting a cycle
+            //cycle for sure exists because of the pigeonhole principle
+            //(n+1 pigeons, n values;)
             slow=nums[slow];
             fast=nums[nums[fast]];
         }
-        slow=0;
-        while(slow!=fast){
+        //now that we have a cycle, we want the starting pt of the cycle, because more than 
+        //one element pts there;
+        slow=0; 
+        while(slow!=fast){ //algo to fidn starting pt of cycle;
             slow=nums[slow];
             fast=nums[fast];
         }
