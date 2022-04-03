9a3f4c482eeee849f3d2faed1409d411d661d546
Prasansa Mapara <94972192+ced7diggory@users.noreply.github.com>
2022-04-03 16:20:39 +0530

    Added solution - LeetHub

--- Minimum Platforms - GFG/minimum-platforms.cpp
+++ Minimum Platforms - GFG/minimum-platforms.cpp
@@ -12,6 +12,18 @@ class Solution{
     //railway station such that no train waits.
    
     int findPlatform(int arr[], int dep[], int n)
+    /*
+    At any given instance of time, same platform can not be used for both
+    departure of a train and arrival of another train. In such cases, we 
+    need different platforms.
+    
+    this is why, we have kept 0 for arrival, and 1 for departure, so in case of 
+    same time, we will first consider arrival, then departure;
+    
+    ex: [[13,15],[1,13]]
+    here ans is two, while for meeting rooms, ans is 1
+    cuz its not possible to have two trains on the same platform at the same time;
+    */
     {
     	vector<pair<int, int>>time;
     	for(int i=0; i<n; i++){
@@ -23,8 +35,8 @@ class Solution{
     	sort(time.begin(), time.end());
     	int ans=0, k=0;
     	for(auto i: time){
-    	    if(i.second==0) k++;
-    	    else k--;
+    	    if(i.second==0) k++; //if arrival, increment the num of platforms
+    	    else k--; //decrement
     	    ans=max(ans, k);
     	}
     	return ans;
