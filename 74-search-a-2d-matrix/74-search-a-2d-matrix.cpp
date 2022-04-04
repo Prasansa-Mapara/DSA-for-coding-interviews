9161a8f294f56be738a4bf4dbbaccdc182136fe0
Prasansa Mapara <94972192+ced7diggory@users.noreply.github.com>
2022-04-05 00:16:47 +0530

    Time: 6 ms (49.06%), Space: 9.5 MB (53.15%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& mat, int target) {
-        //matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
         
         int r=mat.size(), c=mat[0].size();
         int rstart=0, rend=r-1;
