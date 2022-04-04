37d4b8e1cdec6864422e61104bdd89dadf29bc46
Prasansa Mapara <94972192+ced7diggory@users.noreply.github.com>
2022-04-05 00:16:37 +0530

    Time: 3 ms (88.29%), Space: 9.6 MB (53.15%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,30 +1,45 @@
-bool bs(vector<int> &v, int &k){
-    int l=0, r=v.size()-1;
-    while(l<=r){
-        int mid=l+(r-l)/2;
-        if(k==v[mid]){
-            return true;
-        }
-        else if(k>v[mid]){
-            l=mid+1;
-        }
-        else{
-            r=mid-1;
-        }
-    }
-    return false;
-}
-
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& mat, int k) {
-        vector<int> v;
-        int n=mat.size();
-        for(int i=0; i<n; i++){
-            for(int j=0; j<mat[i].size(); j++){
-                v.push_back(mat[i][j]);
+    bool searchMatrix(vector<vector<int>>& mat, int target) {
+        //matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+        
+        int r=mat.size(), c=mat[0].size();
+        int rstart=0, rend=r-1;
+        int rmid, rind=-1;
+        
+        while(rstart<=rend){
+            rmid=rstart+(rend-rstart)/2;
+            
+            if(target<mat[rmid][0]){
+                rend=rmid-1;
+            }
+            else if(target>mat[rmid][c-1]){
+                rstart=rmid+1;
+            }
+            else if(target>=mat[rmid][0] && target<=mat[rmid][c-1]){
+                rind=rmid;
+                break;
+            }
+            else{
+                return false;
             }
         }
-        return bs(v, k);
+        
+        if(rind!=-1){
+            int cstart=0, cend=c-1;
+            while(cstart<=cend){
+                int cmid=cstart+(cend-cstart)/2;
+                if(target==mat[rind][cmid]){
+                    return true;
+                }
+                else if(target>mat[rind][cmid]){
+                    cstart=cmid+1;
+                }
+                else{
+                    cend=cmid-1;
+                }
+            }
+        }
+        return false;
     }
 };
