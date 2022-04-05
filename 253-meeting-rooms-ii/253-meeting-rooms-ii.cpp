d7b9842320ff3b0e02917817e69cb6b31a62e5b4
Prasansa Mapara <94972192+ced7diggory@users.noreply.github.com>
2022-04-05 22:49:58 +0530

    Time: 32 ms (33.52%), Space: 12.9 MB (27.71%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.cpp
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.cpp
@@ -1,27 +1,17 @@
 class Solution {
 public:
     int minMeetingRooms(vector<vector<int>>& intervals) {
-        //[[13,15],[1,13]]
-        //v=[{13,1},{15,0},{1,1},{13,0}]
-        //v=[{1,1},{13,0},{13,1},{15,0}]
-        int rooms=INT_MIN;
-        stack<int> s;
         vector<pair<int, int>> v;
-        for(int i=0; i<intervals.size(); i++){
-            v.push_back({intervals[i][0], 1});
-            v.push_back({intervals[i][1], 0});
+        for(auto i: intervals){
+            v.push_back({i[0], 1});
+            v.push_back({i[1], 0});
         }
         sort(v.begin(), v.end());
+        int rooms=0, cnt=0;
         for(int i=0; i<v.size(); i++){
-            if(v[i].second==1){
-                s.push(1);
-                rooms=max(rooms,(int)s.size());
-            }
-            else{
-                if(!s.empty()){
-                    s.pop();
-                }
-            }
+            if(v[i].second==1) cnt++;
+            else cnt--;
+            rooms=max(cnt, rooms);
         }
         return rooms;
     }
