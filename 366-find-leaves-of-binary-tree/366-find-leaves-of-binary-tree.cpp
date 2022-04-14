32f8cb023350a908a12497aa663c161f96757b70
Prasansa Mapara <94972192+ced7diggory@users.noreply.github.com>
2022-04-14 06:58:26 +0530

    Time: 7 ms (13.59%), Space: 9 MB (44.40%) - LeetHub

---
+++ 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    vector<vector<int>> sol;
+public:
+    int findHeight(TreeNode *curr){
+        if(!curr) return -1;
+        int curH=1+max(findHeight(curr->left), findHeight(curr->right));
+        if(sol.size()<=curH) sol.push_back({});
+        sol[curH].push_back(curr->val);
+        return curH;
+    }
+    
+    vector<vector<int>> findLeaves(TreeNode* root) {
+        if(!root) return sol;
+        int curH=1+max(findHeight(root->left), findHeight(root->right));
+        if(sol.size()<=curH) sol.push_back({});
+        sol[curH].push_back(root->val);
+        return sol;
+    }
+};
