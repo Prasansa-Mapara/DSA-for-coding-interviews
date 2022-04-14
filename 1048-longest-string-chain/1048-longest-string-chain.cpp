ffe19ac164cf15904f5e150b68d6776a1e5c5be9
Prasansa Mapara <94972192+ced7diggory@users.noreply.github.com>
2022-04-14 09:12:28 +0530

    Time: 138 ms (45.87%), Space: 37.2 MB (13.17%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -5,11 +5,10 @@ class Solution {
 public:
     int longestStrChain(vector<string>& words) {
         sort(words.begin(), words.end(), func);
-        int n=words.size();
-        vector<int> dp(n, 1);
-        unordered_map<string, int> hash;
+        int n=words.size(), sol=1;
+        unordered_map<string, int> dp;
         for(int i=0; i<n; i++){
-            hash[words[i]]=i;
+            dp[words[i]]=1;
         }
         for(int i=0; i<n; i++){
             string s=words[i]; 
@@ -17,11 +16,12 @@ public:
             for(int j=0; j<sz; j++){
                 string tmp=s;
                 tmp.erase(tmp.begin()+j);
-                if(hash.find(tmp)!=hash.end()){
-                    dp[i]=max(dp[i], 1+dp[hash[tmp]]);
+                if(dp.find(tmp)!=dp.end()){
+                    dp[s]=max(dp[s], 1+dp[tmp]);
+                    sol=max(sol, dp[s]);
                 }
             }
         }
-        return *max_element(dp.begin(), dp.end());
+        return sol;
     }
 };
