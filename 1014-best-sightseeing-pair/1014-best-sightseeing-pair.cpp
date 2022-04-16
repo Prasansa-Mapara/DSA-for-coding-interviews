268bf1f1e71557577bfab2f15587f51f8f2dcb61
Prasansa Mapara <94972192+ced7diggory@users.noreply.github.com>
2022-04-16 15:18:25 +0530

    Time: 70 ms (57.66%), Space: 39.4 MB (85.25%) - LeetHub

--- 1014-best-sightseeing-pair/1014-best-sightseeing-pair.cpp
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int maxScoreSightseeingPair(vector<int>& v) {
-        int n=v.size(), sol=INT_MIN, tmp=v[0]-1;
+        int n=v.size(), sol=INT_MIN, tmp=v[0]-1; //already taking subtracted value;
         for(int i=1; i<n; i++){
             sol=max(sol, v[i]+tmp);
-            tmp--;
-            tmp=max(tmp, v[i]-1);
+            tmp--; //subtracting for next iteration;
+            tmp=max(tmp, v[i]-1); //already taking subtracted value;
         }
         return sol;
     }
