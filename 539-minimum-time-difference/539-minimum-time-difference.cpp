36b064cac969eee318e3796ecb535e068fa84479
Prasansa Mapara <94972192+ced7diggory@users.noreply.github.com>
2022-04-19 14:28:19 +0530

    Time: 28 ms (26.06%), Space: 13.4 MB (89.70%) - LeetHub

--- 539-minimum-time-difference/539-minimum-time-difference.cpp
+++ 539-minimum-time-difference/539-minimum-time-difference.cpp
@@ -1,17 +1,8 @@
-bool cmp(string s1, string s2){
-    string a=s1.substr(0, s1.find(":")), b=s2.substr(0, s2.find(":"));
-    if(stoi(a)==stoi(b)){
-        string c=s1.substr(s1.find(":")+1), d=s2.substr(s2.find(":")+1);
-        return stoi(c)<stoi(d);
-    }
-    return stoi(a)<stoi(b);
-}
-
 class Solution {
 public:
     int findMinDifference(vector<string>& pts) {
         int n=pts.size(), ans=INT_MAX;
-        sort(pts.begin(), pts.end(), cmp);
+        sort(pts.begin(), pts.end());
         for(int i=1; i<n; i++){
             string s1=pts[i-1], s2=pts[i];
             int h1=stoi(s1.substr(0, s1.find(":"))), m1=stoi(s1.substr(s1.find(":")+1)), a=m1+h1*60;
