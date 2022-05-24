3f88643d36a17771eafa7093a3bb67d692e2735d
Prasansa Mapara <94972192+ced7diggory@users.noreply.github.com>
2022-05-24 15:42:50 +0530

    Time: 3 ms (88.93%), Space: 7.4 MB (42.55%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        int n=s.size(), sol=0, tmp=0;
-        stack<int> st;
-        st.push(-1);
-        for(int i=0; i<n; i++){
-            if(s[i]=='('){
-                st.push(i);
-            }
-            else{
-                st.pop();
-                if(st.empty()){
-                    st.push(i);
+        int n=s.size();
+        if(n<=1) return 0;
+        vector<int> dp(n, 0);
+        //dp[i] will store the len of max valid substring ending at ith index;
+        for(int i=1; i<n; i++){
+            if(s[i]==')'){ //because a valid substring can only end with ')';
+                if(s[i-1]=='('){
+                    if(i>=2) dp[i]=dp[i-2]+2; //obvious;
+                    else dp[i]=2;
                 }
                 else{
-                    sol=max(sol, i-st.top());
+                    int prev=i-dp[i-1]-1; //prev is the index before previous valid substring;
+                    if(prev>=0 && s[prev]=='('){
+                        if(prev>=1) dp[i]=dp[i-1]+2+dp[prev-1]; //if thats opening, then 2 will be added
+                        //along with len of subtring previous to prev, cuz they might be connected now;
+                        else dp[i]=dp[i-1]+2;
+                    }
                 }
             }
         }
-        return sol;
+        return *max_element(dp.begin(), dp.end());
     }
 };
