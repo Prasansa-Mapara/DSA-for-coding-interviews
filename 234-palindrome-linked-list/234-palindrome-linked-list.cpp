95396436818117ab1be7323f86086a328b50ec64
Prasansa Mapara <94972192+ced7diggory@users.noreply.github.com>
2022-05-29 11:10:48 +0530

    Time: 223 ms (91.56%), Space: 114 MB (90.62%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -15,16 +15,16 @@ public:
         while(fast && fast->next){
             slow=slow->next;
             fast=fast->next->next;
-        }
-        ListNode *prev=NULL, *curr=slow, *nex;
-        if(fast) curr=slow->next;
+        } //found mid;
+        ListNode *prev=NULL, *curr=slow, *nex; //in case of even 
+        if(fast) curr=slow->next; //in case of odd, reverse after mid;
         while(curr){
             nex=curr->next;
             curr->next=prev;
             prev=curr;
             curr=nex;
         }
-        while(prev){
+        while(prev){ //now check if before mid and after mid are equal;
             if(head->val==prev->val){
                 head=head->next;
                 prev=prev->next;
