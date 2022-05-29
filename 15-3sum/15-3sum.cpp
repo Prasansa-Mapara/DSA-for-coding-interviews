bdb5d1c965d2a839d37cd04b203e8ef1fda1a0d0
Prasansa Mapara <94972192+ced7diggory@users.noreply.github.com>
2022-05-29 18:37:01 +0530

    Time: 2007 ms (5.01%), Space: 295.7 MB (5.64%) - LeetHub

---
+++ 15-3sum/15-3sum.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n=nums.size();
+        set<vector<int>> hash;
+        vector<vector<int>> sol;
+        sort(nums.begin(), nums.end());
+        for(int i=0; i<n-2; i++){
+            int req=nums[i]*(-1);
+            int j=i+1, k=n-1;
+            while(j<k){
+                int curr=nums[j]+nums[k];
+                if(curr<req){
+                    j++;
+                }
+                else if(curr>req){
+                    k--;
+                }
+                else{
+                    vector<int> v={nums[i], nums[j], nums[k]};
+                    if(hash.find(v)==hash.end()){
+                        hash.insert(v);
+                        sol.push_back(v);
+                    }
+                    j++;
+                }
+            }
+        }
+        return sol;
+    }
+};
