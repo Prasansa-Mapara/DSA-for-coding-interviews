9bb0035b5f5213144a2ea6899d9047ab0a7b37ca
Prasansa Mapara <94972192+ced7diggory@users.noreply.github.com>
2022-05-30 09:54:00 +0530

    Time: 15 ms (95.08%), Space: 12.4 MB (39.30%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.cpp
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
-    int minMeetingRooms(vector<vector<int>>& intervals) {
+    int minMeetingRooms(vector<vector<int>>& time) {
         vector<pair<int, int>> v;
-        for(auto i: intervals){
-            v.push_back({i[0], 1});
-            v.push_back({i[1], 0});
+        int n=time.size();
+        for(int i=0; i<n; i++){
+            v.push_back({time[i][0], 1});
+            v.push_back({time[i][1], 0});
         }
         sort(v.begin(), v.end());
-        int rooms=0, cnt=0;
-        for(int i=0; i<v.size(); i++){
-            if(v[i].second==1) cnt++;
-            else cnt--;
-            rooms=max(cnt, rooms);
+        int  rooms=0, curr=0;
+        n=v.size();
+        for(int i=0; i<n; i++){
+            if(v[i].second==1) curr++;
+            else curr--;
+            rooms=max(rooms, curr);
         }
         return rooms;
     }
