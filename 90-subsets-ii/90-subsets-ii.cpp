71bb9b7788c2b0271afc312d51f8e5414c2a677d
Prasansa Mapara <94972192+ced7diggory@users.noreply.github.com>
2022-05-31 14:10:24 +0530

    Time: 40 ms (5.63%), Space: 22.6 MB (5.16%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -16,16 +16,18 @@ public:
             hash.insert(v);
             return;
         }
-        solve(nums, ind+1, tmp);
-        tmp[nums[ind]]++;
+        solve(nums, ind+1, tmp); //w/o considering this index;
+        tmp[nums[ind]]++;        //considering this index;
         solve(nums, ind+1, tmp);
     }
     
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        map<int, int> tmp;
+        map<int, int> tmp; //here i cant kepe unordered map, cuz then [1,4] and [4,1] will be treated separately;
+        //not directly using a vector, cuz we don't wanna count 1,4 and 4,1 twice, this both should mean the same;
+        //as no duplicate subset and for a subset, (1,4) and (4,1) is same only, since this isn't a subsequence;
         solve(nums, 0, tmp);
         vector<vector<int>> sol;
-        for(auto v: hash){
+        for(auto v: hash){ //used a set so that there will be no duplicates;
             sol.push_back(v);
         }
         return sol;
