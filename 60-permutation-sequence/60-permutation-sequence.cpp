d42080ee0d94dabf2429a7c23eb71420193cd6b5
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-06-01 13:15:57 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (48.38%) - LeetHub

--- 60-permutation-sequence/60-permutation-sequence.cpp
+++ 60-permutation-sequence/60-permutation-sequence.cpp
@@ -4,19 +4,19 @@ public:
         vector<int> fact(n+1);
         fact[0]=1;
         for(int i=1; i<=n; i++){
-            fact[i]=i*fact[i-1];
+            fact[i]=i*fact[i-1]; //store the factorials beforehand;
         }
         string s, sol;
         for(int i=1; i<=n; i++){
             s+=to_string(i);
         }
         while(n){
-            int ind=(k/fact[n-1]);   
-            if((k%fact[n-1])==0) ind--;
+            int ind=(k/fact[n-1]); //this will be the table number that we'll have to consider;  
+            if((k%fact[n-1])==0) ind--; //boundary check;
             sol.push_back(s[ind]); 
             s.erase(s.begin()+ind); 
-            k-=(fact[n-1]*(ind)); 
-            n--;
+            k-=(fact[n-1]*(ind));  //here ind will be table number in 0 based indexing, so there'll be ind number of tables before it, we need to discared those, so multiply it with the table size that is fact[n-1]
+            n--; //so now, n will be 1 less;
         }
         return sol;
     }
