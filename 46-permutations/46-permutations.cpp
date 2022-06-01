961601758d8bb7a33230634f06253bac8f8986b8
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-06-01 15:38:47 +0530

    Time: 8 ms (26.31%), Space: 8.4 MB (15.37%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -2,27 +2,23 @@ class Solution {
 public:
     vector<vector<int>> sol;
     
-    void solve(vector<int> &v, unordered_set<int> hash, vector<int> tmp){
+    void solve(vector<int> v, int ind){
         int n=v.size();
-        if(tmp.size()==n){
-            sol.push_back(tmp);
+        if(ind==n){
+            sol.push_back(v);
             return;
         }
-        for(int i=0; i<n; i++){
-            if(hash.find(v[i])==hash.end()){
-                tmp.push_back(v[i]);
-                hash.insert(v[i]);
-                solve(v, hash, tmp);
-                tmp.pop_back();
-                hash.erase(hash.find(v[i]));
-            }
+        for(int i=ind; i<n; i++){
+            swap(v[i], v[ind]);
+            solve(v, ind+1);
+            swap(v[i], v[ind]);
         }
+        return;
     }
     
     vector<vector<int>> permute(vector<int>& nums) {
-        unordered_set<int> hash;
         vector<int> tmp;
-        solve(nums, hash, tmp);
+        solve(nums, 0);
         return sol;
     }
 };
