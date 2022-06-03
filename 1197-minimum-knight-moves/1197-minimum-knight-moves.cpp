887db5232ef8e2cd53d00139d4c4b11839a45583
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-06-03 18:08:10 +0530

    Time: 20 ms (93.24%), Space: 9.7 MB (92.65%) - LeetHub

--- 1197-minimum-knight-moves/1197-minimum-knight-moves.cpp
+++ 1197-minimum-knight-moves/1197-minimum-knight-moves.cpp
@@ -2,34 +2,39 @@ class Solution {
 public:
     int minKnightMoves(int x, int y) {
         vector<pair<int, int>> dir;
-        dir.push_back({1, 2}); dir.push_back({2, 1});
-        dir.push_back({1, -2}); dir.push_back({2, -1});
-        dir.push_back({-1, 2}); dir.push_back({-2, 1});
+        if(x<0) x=-x;
+        if(y<0) y=-y;
         dir.push_back({-1, -2}); dir.push_back({-2, -1});
         
         queue<pair<int, pair<int, int>>> q;
-        vector<vector<bool>> vis(607, vector<bool>(607, 0));
-        q.push({0, {0, 0}});
+        set<pair<int, int>> vis;
+        q.push({0, {x, y}});
+        int sol=INT_MAX;
         
         while(q.size()){
             int steps=q.front().first; 
-            int xx=q.front().second.first, yy=q.front().second.second;            
+            int xx=q.front().second.first, yy=q.front().second.second;   
+            xx=abs(xx); yy=abs(yy);
             q.pop();
             
-            if(xx==x && yy==y){
+            if((xx==0 && yy==0)){
                 return steps;
             }
+            else if((xx+yy)==2){
+                sol=min(sol, steps+2);
+            }
             else{
-                for(int i=0; i<8; i++){
-                    int xi=xx+dir[i].first+302, yi=yy+dir[i].second+302;
-                    if(vis[xi][yi]==0){
-                        vis[xi][yi]=1;
-                        q.push({steps+1, {xi-302, yi-302}});
+                for(int i=0; i<2; i++){
+                    int xi=xx+dir[i].first, yi=yy+dir[i].second;
+                    xi=abs(xi); yi=abs(yi);
+                    if(vis.find({xi, yi})==vis.end()){
+                        vis.insert({xi, yi});
+                        q.push({steps+1, {xi, yi}});
                     }
                 }
             }
         }
         
-        return -1;
+        return sol;
     }
 };
