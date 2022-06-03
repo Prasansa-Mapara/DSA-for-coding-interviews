349e8daeb0ed2e72fa5f456b9b3cc2e2fedbdd71
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-06-03 18:27:04 +0530

    Time: 40 ms (87.94%), Space: 10.3 MB (92.21%) - LeetHub

--- 1197-minimum-knight-moves/1197-minimum-knight-moves.cpp
+++ 1197-minimum-knight-moves/1197-minimum-knight-moves.cpp
@@ -1,36 +1,33 @@
 class Solution {
 public:
     int minKnightMoves(int x, int y) {
-        vector<pair<int, int>> dir;
-        if(x<0) x=-x;
-        if(y<0) y=-y;
-        dir.push_back({-1, -2}); dir.push_back({-2, -1});
+        //will solve this using BFS;
+        //since the solution will be similar for (x,y), (x,-y), (-x,y), (-x,-y), will reduce the solution space to the first quadrant;
+        //when we'll reach the neighbourhood, it will take 2 steps, till then, if we start from target, 
+        //will only have to move in (-1,-2) and (-2,-1), just to get closer to the origin;
         
+        x=abs(x); y=abs(y);
         queue<pair<int, pair<int, int>>> q;
-        set<pair<int, int>> vis;
         q.push({0, {x, y}});
+        set<pair<int, int>> vis;
         int sol=INT_MAX;
         
         while(q.size()){
-            int steps=q.front().first; 
-            int xx=q.front().second.first, yy=q.front().second.second;   
-            xx=abs(xx); yy=abs(yy);
+            auto it=q.front();
             q.pop();
-            
-            if((xx==0 && yy==0)){
-                return steps;
+            int st=it.first, xx=it.second.first, yy=it.second.second;
+            xx=abs(xx); yy=abs(yy);
+            if(xx==0 && yy==0){
+                return st;
             }
-            else if((xx+yy)==2){
-                sol=min(sol, steps+2);
+            else if((xx+yy)==2){ //in the neighbourhood;
+                sol=min(sol, st+2); //cuz next elements in queue, might end up at (0,0);
             }
             else{
-                for(int i=0; i<2; i++){
-                    int xi=xx+dir[i].first, yi=yy+dir[i].second;
-                    xi=abs(xi); yi=abs(yi);
-                    if(vis.find({xi, yi})==vis.end()){
-                        vis.insert({xi, yi});
-                        q.push({steps+1, {xi, yi}});
-                    }
+                if(vis.find({xx, yy})==vis.end()){
+                    vis.insert({xx, yy});
+                    q.push({st+1, {xx-2, yy-1}});
+                    q.push({st+1, {xx-1, yy-2}}); //these 2 are the only steps we need to take to reach closer to the origin;
                 }
             }
         }
