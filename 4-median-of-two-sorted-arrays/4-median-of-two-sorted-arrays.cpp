60fbd141f576c7d33fbdd4a2a6f9d722803e423b
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-06-06 22:47:05 +0530

    Time: 67 ms (28.57%), Space: 89.3 MB (87.02%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,14 +1,38 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        nums1.insert(nums1.end(), nums2.begin(), nums2.end());
-        sort(nums1.begin(), nums1.end());
-        int n=nums1.size();
-        if(n%2){
-            return (double)nums1[n/2];
+        int m=nums1.size(), n=nums2.size();
+        if(m>n){
+            swap(nums1, nums2);
+            swap(m, n);
         }
-        else{
-            return (double)(nums1[n/2]+nums1[(n/2)-1])/2.0;
+        int lo=0, hi=m;
+        
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2, rem=((m+n+1)/2)-mid;
+            int aL, aR, bL, bR;
+            aL=(mid>0)?(nums1[mid-1]):(INT_MIN);
+            aR=(mid<m)?(nums1[mid]):(INT_MAX);
+            bL=(rem>0)?(nums2[rem-1]):(INT_MIN);
+            bR=(rem<n)?(nums2[rem]):(INT_MAX);
+            
+            if(aL<=bR && bL<=aR){
+                int tot=m+n;
+                if(tot%2){
+                    return (double)max(aL, bL);
+                }
+                else{
+                    return (double)(max(aL, bL)+min(aR, bR))/2.0;
+                }
+            }
+            else if(aL>bR){
+                hi=mid-1;
+            }
+            else{
+                lo=mid+1;
+            }
         }
+        
+        return 0;
     }
 };
