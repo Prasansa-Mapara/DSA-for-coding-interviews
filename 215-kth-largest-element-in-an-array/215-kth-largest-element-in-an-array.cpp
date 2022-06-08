d068dc2b8f894bf0f4c55398d260eba8de3736d5
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-06-08 18:02:21 +0530

    Time: 200 ms (5.04%), Space: 9.9 MB (85.98%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -22,8 +22,26 @@ public:
     
     int findKthLargest(vector<int>& nums, int k) {
         int n=nums.size();
-        int l=0, piv=n-1;
-        return solve(0, piv, n-k, nums); //quick select;
-        //n-k is the index from the start;
+        int l=0, piv=n-1, ind=n-k;
+        while(l<=piv){
+            int j=l;
+            for(int i=l; i<piv; i++){
+                if(nums[i]<=nums[piv]){
+                    swap(nums[i], nums[j]);
+                    j++;
+                }
+            }
+            swap(nums[piv], nums[j]);
+            if(j==ind){
+                return nums[j];
+            }
+            else if(j<ind){
+                l=j+1;
+            }
+            else{
+                piv=j-1;
+            }
+        }
+        return -1;
     }
 };
