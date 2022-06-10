97e29319423f303ad88e36a4e298d75e6027c839
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-06-10 19:29:43 +0530

    Time: 190 ms (85.20%), Space: 79.6 MB (54.75%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -1,27 +1,34 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
+        //if a smaller rectangle comes, then we'll be in trouble; 
+        //so we'll strictly maintain an increasing order;
+        
+        stack<pair<int, int>> st; //height->index;
+        //not storing indices directly cuz as we remove the larger height elements, we'll need to store that last index cuz it can go both left and right sides;
+        
         int n=heights.size(), area=0;
-        stack<pair<int, int>> st; 
         for(int i=0; i<n; i++){
-            if(st.empty() || st.top().first<=heights[i]){
+            if(st.empty() || heights[i]>=st.top().first){
                 st.push({heights[i], i});
             }
             else{
                 int j;
                 while(st.size() && heights[i]<st.top().first){
-                    j=st.top().second;
-                    int curr=st.top().first;
+                    j=st.top().second; //curr element can go upto j in left side as its height is less than height of j;
+                    //we found the right index;
+                    area=max(area, st.top().first*(i-j));
                     st.pop();
-                    area=max(area, curr*(i-j));
                 }
                 st.push({heights[i], j});
             }
         }
+        
         while(st.size()){
             area=max(area, st.top().first*(n-st.top().second));
             st.pop();
         }
+        
         return area;
     }
 };
