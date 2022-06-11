6326494bf46282b6332d306ce854406f8dbfdb69
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-06-11 23:26:32 +0530

    Time: 374 ms (25.17%), Space: 84.1 MB (90.82%) - LeetHub

--- 901-online-stock-span/901-online-stock-span.cpp
+++ 901-online-stock-span/901-online-stock-span.cpp
@@ -1,26 +1,19 @@
 class StockSpanner {
 public:
-    stack<pair<int, int>> st;
-    int i=0;
-    
+    stack<pair<int, int>> st;\
+        
     StockSpanner() {
-        //we want previous prices to be less than or equal to current price;
-        //so will maintain a monotonically decreasing stack and store indices in the stack;
-        //so whenever there's an element greater than top of stack, will keep popping, 
-        //till tht element is less than top of stack, then will update result;
+        
     }
     
     int next(int price) {
-        while(st.size() && price>=st.top().first){ //maintain a monotonically decreasing stack;
+        int sol=1;
+        while(st.size() && price>=st.top().first){
+            sol+=st.top().second; //cuz prev price was less than or equal to curr price;
             st.pop();
-        }
-        int x=-1;
-        if(st.size()){
-            x=st.top().second;
-        }
-        st.push({price, i});
-        i++;
-        return i-x-1;
+        } 
+        st.push({price, sol});
+        return sol;
     }
 };
 
