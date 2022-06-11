63dc1c9105a18787a51c3d272292de38da4f7967
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-06-11 17:01:01 +0530

    Time: 535 ms (25.76%), Space: 134.6 MB (52.43%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -2,21 +2,21 @@ class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         int n=nums.size();
-        //since numbers are not distinct, we'll store indices in the queue;
-        //we'll maintain a decreasing order queue, so max will always be at head;
-        vector<int> sol;
         deque<int> q;
-        for(int i=0; i<n; i++){
-            if(q.size() && (i-q.front())==k){
-                q.pop_front(); //cuz size of sliding window is k, so remove previous elements;
-            }
-            while(q.size() && nums[q.back()]<nums[i]){
-                q.pop_back(); //we want to maintain a descending order;
+        for(int i=0; i<k-1; i++){
+            while(q.size() && nums[i]>nums[q.back()]){
+                q.pop_back();
             }
             q.push_back(i);
-            if((i-k+1)>=0){
-                sol.push_back(nums[q.front()]);
+        }
+        vector<int> sol;
+        for(int i=k-1; i<n; i++){
+            while(q.size() && nums[i]>nums[q.back()]){
+                q.pop_back();
             }
+            q.push_back(i);
+            if((i-q.front()==k)) q.pop_front(); //size of sliding window is k;
+            sol.push_back(nums[q.front()]);
         }
         return sol;
     }
