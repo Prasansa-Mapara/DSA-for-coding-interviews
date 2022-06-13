e9613826650797a69c8449bc0a27b8e73f121860
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-06-13 12:10:35 +0530

    Time: 37 ms (10.28%), Space: 15.4 MB (6.05%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -1,24 +1,32 @@
-//can be solved in linear time using bucket sort; 
-
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        int n=nums.size();
-        vector<vector<int>> buck(n+1);
+        //one way is to use min heap, but that will be costly;
+        //another way is to use quick selecty, but it will be O(n^2) in worst case;
+        //so best way is to use bucket sort;
+        
         unordered_map<int, int> hash;
         for(auto i: nums){
             hash[i]++;
         }
-        for(auto i=hash.begin(); i!=hash.end(); i++){
-            buck[i->second].push_back(i->first);
+        //1->3, 2->2, 3->1
+        //max frequency of any element will be n, so store these elements in a bucket(cntr array) 
+        //and then iterate from the back;
+        int n=nums.size();
+        vector<vector<int>> buck(n+1);
+        
+        for(auto it=hash.begin(); it!=hash.end(); it++){
+            buck[it->second].push_back(it->first);
         }
+        
         vector<int> sol;
-        for(int i=n; i>=0 && k; i--){
-            for(int j=0; j<buck[i].size() && k; j++){
+        for(int i=n; i>0; i--){
+            for(int j=0; j<buck[i].size(); j++){
                 sol.push_back(buck[i][j]);
-                k--;
+                if(sol.size()==k) return sol;
             }
         }
+        
         return sol;
     }
 };
