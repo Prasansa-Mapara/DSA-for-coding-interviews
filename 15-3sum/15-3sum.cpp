4899a38dad2eabc501255f706f800929c8f3e2df
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-06-13 13:22:50 +0530

    Time: 64 ms (98.56%), Space: 20.1 MB (52.60%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int n=nums.size();
-        vector<vector<int>> sol;
+        //tc will be O(n^2)
+        //first sol for interviewer:
+        
+        int n=nums.size(); 
         sort(nums.begin(), nums.end());
-        for(int i=0; i<n-2; i++){
-            int req=-nums[i];
-            int j=i+1, k=n-1;
-            while(j<k){
-                int curr=nums[j]+nums[k];
-                if(curr<req){
-                    j++;
+        vector<vector<int>> sol;
+        for(int i=0; i<n; i++){
+            if(i && nums[i]==nums[i-1]) continue; //we don't want duplicates;
+            if(nums[i]>0) break; //as array is sorted;
+            int l=i+1, r=n-1; //2 ptrs;
+            while(l<r){
+                int x=nums[i]+nums[l]+nums[r];
+                if(x==0){
+                    sol.push_back({nums[i], nums[l], nums[r]});
+                    l++; r--;
+                    while(l<r && nums[l]==nums[l-1]) l++;
                 }
-                else if(curr>req){
-                    k--;
+                else if(x>0){
+                    r--;
                 }
                 else{
-                    vector<int> v={nums[i], nums[j], nums[k]};
-                    sol.push_back(v);
-                    while(j<k && nums[j]==v[1]){
-                        j++;
-                    }
-                    while(k>j && nums[k]==v[2]){
-                        k--;
-                    }
+                    l++;
                 }
-            }
-            while((i+1)<(n-2) && nums[i+1]==nums[i]){
-                i++;
             }
         }
         return sol;
