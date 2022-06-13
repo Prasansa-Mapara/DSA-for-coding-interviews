131756f618f48705a7d67b53be78a747f2558d2d
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-06-13 11:50:27 +0530

    Time: 182 ms (6.78%), Space: 27.8 MB (8.36%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> hash;
+        //this approach ill take O(NKlogK);
+       /* unordered_map<string, vector<string>> hash;
         for(auto s: strs){
             string tmp=s;
             sort(tmp.begin(), tmp.end());
@@ -11,6 +12,29 @@ public:
         for(auto i=hash.begin(); i!=hash.end(); i++){
             sol.push_back(i->second);
         }
+        return sol;*/
+        
+        //we can still get a better solution, if we store a cntr array instead of this;
+        
+        unordered_map<string, vector<string>> hash;
+        vector<vector<string>> sol;
+        
+        for(auto s: strs){
+            vector<int> cntr(26, 0);
+            for(auto c: s){
+                cntr[c-'a']++;
+            }
+            string tmp;
+            for(int i=0; i<26; i++){
+                tmp+=to_string(cntr[i])+"#";
+            }
+            hash[tmp].push_back(s);
+        }
+        
+        for(auto it=hash.begin(); it!=hash.end(); it++){
+            sol.push_back(it->second);
+        }
+        
         return sol;
     }
 };
