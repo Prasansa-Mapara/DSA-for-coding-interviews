120e2725cd9f6104e665623fd6380033ce7acae4
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-06-13 11:27:01 +0530

    Time: 12 ms (61.83%), Space: 7.2 MB (78.37%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,9 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end()); 
-        sort(t.begin(), t.end());
-        if(s==t) return 1;
+        //since s and t contains lowercase chars, we can keep a cntr array of size 26;
+        vector<int> cnt1(26, 0), cnt2(26, 0);
+        int n=s.size(); 
+        if(t.size()!=n) return 0;
+        
+        for(int i=0; i<n; i++){ 
+            cnt1[s[i]-'a']++;
+            cnt2[t[i]-'a']++;
+        }
+        
+        if(cnt1==cnt2) return 1;
         return 0;
     }
 };
