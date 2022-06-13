417577c5affa91cab83fda18d65cf1ba129b08cf
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-06-13 23:43:49 +0530

    Time: 38 ms (32.49%), Space: 10.8 MB (27.99%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int n=s.size(), sol=1, i=0;
-        if(n==0) return 0; //if there's no char then it cant repeat;
-        unordered_map<char, int> hash;
-        hash[s[0]]=0;
-        for(int j=1; j<n; j++){ 
-            if(hash.find(s[j])!=hash.end()){
-                i=max(i, hash[s[j]]+1); //if that char is found before, then i will be upated; consider tc "abba", for max;
+        int sol=1, n=s.size();
+        if(n==0) return 0;
+        int l=0, r=0; 
+        unordered_set<char> hash;
+        hash.insert(s[l]);
+        r=l+1;
+        while(r<n){
+            if(hash.find(s[r])==hash.end()){
+                hash.insert(s[r]);
+                sol=max(sol, r-l+1);
+                r++;
             }
-            hash[s[j]]=j; //update char to curr location;
-            sol=max(sol, j-i+1);
+            else{
+                hash.erase(hash.find(s[l]));
+                l++;
+            }
         }
         return sol;
     }
