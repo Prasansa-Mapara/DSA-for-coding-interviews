36db75135a1b575aca430bf708a49604a49a8575
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-06-13 12:51:30 +0530

    Time: 23 ms (36.07%), Space: 15.7 MB (42.91%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -5,7 +5,8 @@ public:
         //so will have to use 2 ptrs;
         int i=0, j=nums.size()-1;
         while(i<j){
-            int x=nums[i]+nums[j];
+            int x=nums[i]+nums[j]; //if numbers were large, we can use long instead of int;
+            //long supports upto 2^63-1;
             if(x==k){
                 return {i+1, j+1};
             }
