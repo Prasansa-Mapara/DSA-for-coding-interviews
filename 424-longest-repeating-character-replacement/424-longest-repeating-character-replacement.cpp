94e9c509e966f2d68884c309913cff698c9a0be0
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-06-14 01:05:44 +0530

    Time: 27 ms (31.11%), Space: 6.9 MB (98.12%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -3,21 +3,20 @@ public:
     int characterReplacement(string s, int k) {
         int l=0, r, n=s.size(), cnt=0, sol=0;
         unordered_map<char, int> hash;
+        
         for(int r=0; r<n; r++){
-            hash[s[r]]++;
-            cnt=max(cnt, hash[s[r]]); //cnt will be the max frequency of an element in thsi window; 
+            hash[s[r]]++; //update frequency of curr element; 
+            cnt=max(cnt, hash[s[r]]); //cnt will be the max frequency of an element in this window; 
             //we are not updating cnt with every iteration, cuz for sol to change, cnt will have to be increased;
             //so in case cnt remains the same, sol will also remain the same; 
             int x=(r-l+1)-cnt; //num of chars to replace in this window will be total window size- number of characters that appear most frequently; 
-            while(x>k){
+            if(x>k){
                 hash[s[l]]--;
-                l++;
-                x=(r-l+1)-cnt; 
+                l++; //length will have to decrease, so sol will remain same; 
             }
-            if(x<=k){
-                sol=max(sol, r-l+1);
-            }
+            sol=max(sol, r-l+1); //it is easy to observe that sol will change only when cnt is incremented; 
         }
+        
         return sol;
     }
 };
