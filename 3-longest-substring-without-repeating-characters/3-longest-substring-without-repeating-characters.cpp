8dd85246bd42f936151f00152d05d6d6ab01e890
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-06-13 23:57:39 +0530

    Time: 24 ms (54.24%), Space: 8.4 MB (46.78%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int sol=1, n=s.size();
+        int n=s.size(), l=0, r=l+1, sol=1; //because each element is a substring
         if(n==0) return 0;
-        int l=0, r=0; 
-        unordered_set<char> hash;
-        hash.insert(s[l]);
-        r=l+1;
+        unordered_map<char, int> hash;
+        hash[s[l]]=l;
         while(r<n){
-            if(hash.find(s[r])==hash.end()){
-                hash.insert(s[r]);
-                sol=max(sol, r-l+1);
-                r++;
+            if(hash.find(s[r])!=hash.end()){
+                l=max(l, hash[s[r]]+1); //current character (s[r]) was first found at hash[s[r]] index;
+                //so we'll move one step ahead; now [l,r] won't have any duplicate characters;
             }
-            else{
-                hash.erase(hash.find(s[l]));
-                l++;
-            }
+            hash[s[r]]=r;
+            r++; 
+            sol=max(sol, r-l); 
         }
         return sol;
     }
