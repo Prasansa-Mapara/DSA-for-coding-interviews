c63310befd9920f73ca8dcafc180cfd8196a4c01
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-06-16 01:30:59 +0530

    Time: 39 ms (61.57%), Space: 23.3 MB (12.92%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -11,17 +11,25 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(!root) return NULL;
-        int x=p->val, y=q->val, k=root->val;
-        if(x==k || y==k) return root;
-        if(x<k && y<k){ //both are on left side;
-            return lowestCommonAncestor(root->left, p, q);
+        //recursive takes O(n) space;
+        //so doing it in O(1) space;
+        
+        while(root){
+            int x=p->val, y=q->val, k=root->val;
+            if(x==k || y==k){
+                return root;
+            }
+            else if(x<k && y<k){ //both on left side;
+                root=root->left;
+            }
+            else if(x>k && y>k){ //both on right side;
+                root=root->right;
+            }
+            else{ //both on opposite sides;
+                return root;
+            }
         }
-        else if((x<k && y>k) || (x>k && y<k)){ //both are on opp. side;
-            return root;
-        }
-        else{
-            return lowestCommonAncestor(root->right, p, q);
-        }
+        
+        return root;
     }
 };
