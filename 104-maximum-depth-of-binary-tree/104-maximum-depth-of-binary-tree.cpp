79dfa5a8b4d37eed73a2ed9cbe36faaff64cf2e1
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-06-15 18:04:29 +0530

    Time: 12 ms (59.84%), Space: 18.8 MB (89.53%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cpp
@@ -13,6 +13,21 @@ class Solution {
 public:
     int maxDepth(TreeNode* root) {
         if(!root) return 0;
-        return 1+max(maxDepth(root->left), maxDepth(root->right));
+        stack<pair<TreeNode*, int>> st;
+        st.push({root, 1});
+        int sol=0;
+        while(st.size()){
+            TreeNode *curr=st.top().first;
+            int depp=st.top().second;
+            st.pop();
+            sol=max(sol, depp);
+            if(curr->left){
+                st.push({curr->left, depp+1});
+            }
+            if(curr->right){
+                st.push({curr->right, depp+1});
+            }
+        }
+        return sol;
     }
 };
