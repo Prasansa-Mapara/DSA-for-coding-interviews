e842f5c2e9bc5c9faa46f3634f282a63d2215235
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-06-16 23:58:20 +0530

    Time: 1039 ms (7.82%), Space: 79.5 MB (20.79%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -1,14 +1,16 @@
-#define piv pair<long long int, vector<int>>
+#define piv pair<long long int, vector<int>> 
+
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
         vector<vector<int>> sol;
-        priority_queue<piv, vector<piv>, greater<piv>> pq;
-        for(auto x: points){
-            long long int tmp=x[0]*x[0]+x[1]*x[1];
-            pq.push({tmp, x});
+        priority_queue<piv> pq;
+        for(auto i: points){
+            long long int dist=i[0]*i[0]+i[1]*i[1];
+            pq.push({dist, i});
+            if(pq.size()>k) pq.pop();
         }
-        while(k--){
+        while(pq.size()){
             sol.push_back(pq.top().second);
             pq.pop();
         }
