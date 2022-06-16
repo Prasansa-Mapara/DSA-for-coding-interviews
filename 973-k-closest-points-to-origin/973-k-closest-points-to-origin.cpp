4c33f89a9b4f0ca977b4c9a0cb472f70a8767b0c
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-06-17 01:25:25 +0530

    Time: 332 ms (56.85%), Space: 85.2 MB (13.78%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -1,11 +1,14 @@
-bool cmp(vector<int> x, vector<int> y){
-    return (x[0]*x[0]+x[1]*x[1])<(y[0]*y[0]+y[1]*y[1]);
+bool cmp(vector<int> a, vector<int> b){
+    return a[0]*a[0]+a[1]*a[1]<b[0]*b[0]+b[1]*b[1];
 }
 
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& pts, int k) {
-        nth_element(pts.begin(), pts.begin()+k, pts.end(), cmp);
+        //can do using quick select; 
+        //it has a cute stl implementation, so will use it :)
+        
+        nth_element(pts.begin(), pts.begin()+k, pts.end(), cmp); //here it will be one indexed, so with k its actually k-1;
         return vector<vector<int>> (pts.begin(), pts.begin()+k);
     }
 };
