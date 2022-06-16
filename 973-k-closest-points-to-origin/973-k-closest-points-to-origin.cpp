b830cff5458675499ccdc8103a83c1fcac9ea2b1
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-06-17 01:10:15 +0530

    Time: 437 ms (34.76%), Space: 85.2 MB (13.78%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -1,19 +1,11 @@
-#define piv pair<long long int, vector<int>> 
+bool cmp(vector<int> x, vector<int> y){
+    return (x[0]*x[0]+x[1]*x[1])<(y[0]*y[0]+y[1]*y[1]);
+}
 
 class Solution {
 public:
-    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        vector<vector<int>> sol;
-        priority_queue<piv> pq;
-        for(auto i: points){
-            long long int dist=i[0]*i[0]+i[1]*i[1];
-            pq.push({dist, i});
-            if(pq.size()>k) pq.pop();
-        }
-        while(pq.size()){
-            sol.push_back(pq.top().second);
-            pq.pop();
-        }
-        return sol;
+    vector<vector<int>> kClosest(vector<vector<int>>& pts, int k) {
+        nth_element(pts.begin(), pts.begin()+k, pts.end(), cmp);
+        return vector<vector<int>> (pts.begin(), pts.begin()+k);
     }
 };
