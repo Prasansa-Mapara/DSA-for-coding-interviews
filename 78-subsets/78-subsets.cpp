632cf0506ff431e2e5e5a7b269eb6b0950ae4771
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-06-17 14:16:23 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (46.19%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     vector<vector<int>> sol;
-    //set<vector<int>> hash;
     
     void solve(vector<int> &nums, int curr, vector<int> tmp){
         int n=nums.size();
-        //if(hash.find(tmp)!=hash.end()) return;
         sol.push_back(tmp);
-        //hash.insert(tmp);
         for(int i=curr; i<n; i++){
             tmp.push_back(nums[i]);
             solve(nums, i+1, tmp);
