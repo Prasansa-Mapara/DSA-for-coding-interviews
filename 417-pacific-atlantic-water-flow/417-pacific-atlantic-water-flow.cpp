9c9e74b8fda2e36819f53d24a5268c28994a43c1
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-06-18 01:06:46 +0530

    Time: 81 ms (34.88%), Space: 19.1 MB (45.89%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
@@ -1,47 +1,43 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>> &v, vector<vector<int>> &ocean, int i, int j){
-        int r=v.size(), c=v[0].size(); 
-        if(ocean[i][j]){
-            return;
-        }
-        ocean[i][j]=1; //it can reach the ocean and its visited;
+    void dfs(vector<vector<int>> &mat, vector<vector<int>> &ocean, int i, int j){
+        if(ocean[i][j]) return;
+        int r=mat.size(), c=mat[0].size();
+        ocean[i][j]=1; //we've set the condition such that it'll always be feasible for this cell to visit the ocean;
         
-        if((i+1)<r && v[i+1][j]>=v[i][j]){
-            dfs(v, ocean, i+1, j);
+        if(i+1<r && mat[i+1][j]>=mat[i][j]){ //water can flow from i+1th cell to ith cell;
+            dfs(mat, ocean, i+1, j); 
         }
-        if((i-1)>=0 && v[i-1][j]>=v[i][j]){
-            dfs(v, ocean, i-1, j);
+        if(i-1>=0 && mat[i-1][j]>=mat[i][j]){
+            dfs(mat, ocean, i-1, j);
         }
-        if((j+1)<c && v[i][j+1]>=v[i][j]){
-            dfs(v, ocean, i, j+1);
+        if(j+1<c && mat[i][j+1]>=mat[i][j]){
+            dfs(mat, ocean, i, j+1);
         }
-        if((j-1)>=0 && v[i][j-1]>=v[i][j]){
-            dfs(v, ocean, i, j-1);
+        if(j-1>=0 && mat[i][j-1]>=mat[i][j]){
+            dfs(mat, ocean, i, j-1);
         }
     }
     
-    vector<vector<int>> pacificAtlantic(vector<vector<int>>& v) {
-        int r=v.size(), c=v[0].size();
-        //we know tht cells in first row n first col can reach pac ocean;
-        //also, cells in last row n last col can reach atl ocean;
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& mat) {
+        int r=mat.size(), c=mat[0].size();
         vector<vector<int>> pac(r, vector<int>(c, 0)), atl=pac;
-        
+        //flows through pac if i==0 or j==0;
+        //flows through atl if i==(r-1) or j==(c-1);
         for(int i=0; i<r; i++){
             for(int j=0; j<c; j++){
-                if(j==0 || i==0){ //if this cell can reach pac, then check for its neighbors;
-                    dfs(v, pac, i, j); 
+                if(i==0 || j==0){
+                    dfs(mat, pac, i, j);
                 }
-                if(i==(r-1) || j==(c-1)){ //if this cell can reach atl, then check for its neighbors;
-                    dfs(v, atl, i, j);
+                if(i==r-1 || j==c-1){
+                    dfs(mat, atl, i, j);
                 }
             }
         }
-        
         vector<vector<int>> sol;
         for(int i=0; i<r; i++){
             for(int j=0; j<c; j++){
-                if(pac[i][j] && atl[i][j]){
+                if(atl[i][j] && pac[i][j]){
                     sol.push_back({i, j});
                 }
             }
