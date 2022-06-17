2283d3ae959c9aaf399bbda512a4f215cc990171
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-06-17 15:02:11 +0530

    Time: 11 ms (27.17%), Space: 8 MB (42.54%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,35 +1,22 @@
 class Solution {
 public:
-    set<vector<int>> hash;
+    vector<vector<int>> sol;
     
-    void solve(vector<int> &nums, int ind, map<int, int> tmp){
+    void solve(vector<int> &nums, int curr, vector<int> tmp){
+        sol.push_back(tmp);
         int n=nums.size();
-        if(ind==n){
-            vector<int> v;
-            for(auto i: tmp){
-                int j=i.second;
-                while(j>0){
-                    v.push_back(i.first);
-                    j--;
-                }
-            }
-            hash.insert(v);
-            return;
+        for(int i=curr; i<n; i++){
+            if(i!=curr && nums[i]==nums[i-1]) continue; //we dont want any duplicates;
+            tmp.push_back(nums[i]);
+            solve(nums, i+1, tmp);
+            tmp.pop_back();
         }
-        solve(nums, ind+1, tmp); //w/o considering this index;
-        tmp[nums[ind]]++;        //considering this index;
-        solve(nums, ind+1, tmp);
     }
     
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        map<int, int> tmp; //here i cant kepe unordered map, cuz then [1,4] and [4,1] will be treated separately;
-        //not directly using a vector, cuz we don't wanna count 1,4 and 4,1 twice, this both should mean the same;
-        //as no duplicate subset and for a subset, (1,4) and (4,1) is same only, since this isn't a subsequence;
+        vector<int> tmp;
+        sort(nums.begin(), nums.end());
         solve(nums, 0, tmp);
-        vector<vector<int>> sol;
-        for(auto v: hash){ //used a set so that there will be no duplicates;
-            sol.push_back(v);
-        }
         return sol;
     }
 };
