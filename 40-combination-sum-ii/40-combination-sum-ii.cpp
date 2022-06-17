1874553ea8d29421a6d3fd554adb0a1a0d29fd16
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-06-17 16:51:20 +0530

    Time: 23 ms (17.69%), Space: 11.6 MB (25.61%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.cpp
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -2,27 +2,28 @@ class Solution {
 public:
     vector<vector<int>> sol;
     
-    void solve(vector<int> &v, int k, vector<int> &tmp, int ind){
+    void solve(vector<int> &nums, int curr, vector<int> tmp, int k){
         if(k==0){
             sol.push_back(tmp);
             return;
         }
-        int n=v.size();
-        for(int i=ind; i<n; i++){
-            if(i>ind && v[i]==v[i-1]) continue;
-            if(v[i]>k){
-                return;
+        else if(k<0) return;
+        int n=nums.size();
+        for(int i=curr; i<n; i++){
+            if(i!=curr && nums[i]==nums[i-1]) continue;
+            if((k-nums[i])>=0){
+                tmp.push_back(nums[i]);
+                solve(nums, i+1, tmp, k-nums[i]);
+                tmp.pop_back();
             }
-            tmp.push_back(v[i]);
-            solve(v, k-v[i], tmp, i+1);
-            tmp.pop_back();
+            else return;
         }
     }
     
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+    vector<vector<int>> combinationSum2(vector<int>& v, int k) {
         vector<int> tmp;
-        sort(candidates.begin(), candidates.end());
-        solve(candidates, target, tmp, 0);
+        sort(v.begin(), v.end());
+        solve(v, 0, tmp, k);
         return sol;
     }
 };
