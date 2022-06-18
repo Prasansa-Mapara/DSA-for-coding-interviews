d9c131082b317a938a1acba5a5b501bd87033911
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-06-18 12:07:08 +0530

    Time: 15 ms (33.44%), Space: 13.1 MB (48.44%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -1,50 +1,54 @@
 class Solution {
 public:
-    int orangesRotting(vector<vector<int>> mat) {
-        int r=mat.size(), c=mat[0].size();
-        queue<pair<int, int>> q;
-        int fresh=0;
+    int orangesRotting(vector<vector<int>>& mat) {
+        //we doing bfs here;
+        queue<pair<int, int>> q; //it'll we store coordinates;
+        int r=mat.size(), c=mat[0].size(), fresh=0, mins=0;
+        //push all rotten oranges to the queue, cuz in first min, they will all convert thier neighboring fresh oranges into rotten;
         for(int i=0; i<r; i++){
             for(int j=0; j<c; j++){
-                if(mat[i][j]==2){
+                if(mat[i][j]==1) fresh++;
+                else if(mat[i][j]==2){
                     q.push({i, j});
-                }
-                else if(mat[i][j]==1){
-                    fresh++;
                 }
             }
         }
-        if(fresh==0) return 0;
-        int mins=0;
+        if(!fresh) return 0;
         while(q.size()){
             int n=q.size();
             mins++;
             while(n--){
                 int i=q.front().first, j=q.front().second;
                 q.pop();
-                if((i+1)<r && mat[i+1][j]==1){
-                    fresh--;
+                //push all fresh oranges adjacent to it, as now they are rotten;
+                if(i+1<r && mat[i+1][j]==1){
+                    fresh--; //now its rotten;
                     mat[i+1][j]=2;
                     q.push({i+1, j});
                 }
-                if((i-1)>=0 && mat[i-1][j]==1){
-                    fresh--;
+                if(i-1>=0 && mat[i-1][j]==1){
+                    fresh--; 
                     mat[i-1][j]=2;
                     q.push({i-1, j});
                 }
-                if((j+1)<c && mat[i][j+1]==1){
+                if(j+1<c && mat[i][j+1]==1){
                     fresh--;
                     mat[i][j+1]=2;
                     q.push({i, j+1});
                 }
-                if((j-1)>=0 && mat[i][j-1]==1){
+                if(j-1>=0 && mat[i][j-1]==1){
                     fresh--;
                     mat[i][j-1]=2;
                     q.push({i, j-1});
                 }
-                if(fresh==0) return mins;
+                if(!fresh){
+                    return mins;
+                }
             }
         }
-        return -1;
+        if(fresh){ //if still there are any fresh oranges left, then return -1;
+            return -1;
+        }
+        return mins;
     }
 };
