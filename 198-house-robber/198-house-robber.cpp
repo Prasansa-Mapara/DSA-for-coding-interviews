0f071fbbbea3e76c6713cdc25f88b38aad81cec4
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-06-19 21:02:57 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (75.50%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -3,11 +3,8 @@ public:
     int rob(vector<int>& nums) {
         int n=nums.size(); 
         for(int i=2; i<n; i++){
-            int mx=INT_MIN;
-            for(int j=i-2; j>=0; j--){
-                mx=max(mx, nums[j]);
-            }
-            nums[i]+=mx;
+           if(i-3>=0) nums[i]+=max(nums[i-2], nums[i-3]);
+           else nums[i]+=nums[i-2];
         }
         return *max_element(nums.begin(), nums.end());
     }
