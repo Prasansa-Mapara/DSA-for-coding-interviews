b7c994753851974d7317aa2a733b6744180ee5de
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-06-19 18:38:55 +0530

    Time: 193 ms (78.09%), Space: 10.7 MB (84.70%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
     int minCostConnectPoints(vector<vector<int>>& pts) {
-        //since the points are fixed, we dont have to keep storing them again and again;
-        int n=pts.size(), sol=0;
-        vector<int> dist(n, INT_MAX);
-        dist[0]=0;
-        int ind=0; //curr minimum dist, and its corresponding index;
+        int n=pts.size();
+        vector<int> dist(n, INT_MAX); //this will store the minimum distance to reach the pt;
         unordered_set<int> hash;
+        dist[0]=0;
+        hash.insert(0);
+        int j=0, sol=0;
         while(hash.size()<n){
-            int mn=INT_MAX, j;
-            hash.insert(ind);
+            int currMin=INT_MAX, ind; //this will store the min distance and its index;
+            hash.insert(j);
             for(int i=0; i<n; i++){
                 if(hash.find(i)!=hash.end()) continue;
-                int curr=abs(pts[i][0]-pts[ind][0])+abs(pts[i][1]-pts[ind][1]);
-                if(curr<dist[i]){
-                    dist[i]=curr;
-                }
-                if(dist[i]<mn){
-                    mn=dist[i];
-                    j=i;
+                int currDist=abs(pts[i][0]-pts[j][0])+abs(pts[i][1]-pts[j][1]);
+                dist[i]=min(dist[i], currDist);
+                if(dist[i]<currMin){
+                    currMin=dist[i];
+                    ind=i;
                 }
             }
-            if(mn==INT_MAX) break;
-            sol+=mn;
-            ind=j;
+            if(currMin==INT_MAX) break;
+            sol+=currMin;
+            j=ind;
         }
         return sol;
     }
