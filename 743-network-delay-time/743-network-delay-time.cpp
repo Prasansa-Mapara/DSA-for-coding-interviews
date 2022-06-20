cf1f10e3fa59507ba94eea636152a5e915f3b0fe
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-06-20 12:30:10 +0530

    Time: 344 ms (12.85%), Space: 45.7 MB (17.27%) - LeetHub

---
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -0,0 +1,35 @@
+#define pii pair<int, int>
+#define pb push_back
+
+class Solution {
+public:
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        vector<vector<pii>> adj(n+1);
+        for(auto x: times){
+            adj[x[0]].pb({x[2], x[1]});
+        }
+        priority_queue<pii, vector<pii>, greater<pii>> pq; //min time will be first;
+        for(auto x: adj[k]){
+            pq.push(x);
+        }
+        int sol=0;
+        unordered_set<int> hash;
+        hash.insert(k);
+        while(pq.size()){
+            int time=pq.top().first, node=pq.top().second;
+            pq.pop();
+            if(hash.find(node)!=hash.end()){ //we've seen this node before;
+                continue;
+            }
+            hash.insert(node);
+            for(auto x: adj[node]){
+                pq.push({x.first+time, x.second});
+            }
+            sol=max(sol, time);
+            if(hash.size()==n){
+                return sol;
+            }
+        }
+        return -1;
+    }
+};
