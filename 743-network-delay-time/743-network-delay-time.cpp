f4bac8f4afd3f0f053627dd177d7acdec26bda4d
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-06-20 12:42:13 +0530

    Time: 215 ms (34.66%), Space: 44.7 MB (19.51%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,56 +1,35 @@
+#define pii pair<int, int>
+
 class Solution {
 public:
-    // Adjacency list, defined it as per the maximum number of nodes
-    // But can be defined with the input size as well
-    vector<pair<int, int>> adj[101];
-    
-    void BFS(vector<int>& signalReceivedAt, int k) {
-        queue<int> q;
-        q.push(k);
-        
-        // Time for starting node is 0
-        signalReceivedAt[k] = 0;
-        
-        while (!q.empty()) {
-            int currNode = q.front(); 
-            q.pop();
-            
-            // Broadcast the signal to adjacent nodes
-            for (pair<int, int> edge : adj[currNode]) {
-                int time = edge.first;
-                int neighborNode = edge.second;
-                
-                int arrivalTime = signalReceivedAt[currNode] + time;
-                if (signalReceivedAt[neighborNode] > arrivalTime) {
-                    // Fastest signal time for neighborNode so far
-                    // signalReceivedAt[currNode] + time : 
-                    // time when signal reaches neighborNode
-                    signalReceivedAt[neighborNode] = arrivalTime;
-                    q.push(neighborNode);
-                }
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        vector<vector<pii>> adj(n+1);
+        for(auto x: times){
+            adj[x[0]].push_back({x[2], x[1]});
+        }
+        priority_queue<pii, vector<pii>, greater<pii>> pq; //min time will be first;
+        for(auto x: adj[k]){
+            pq.push(x); //this way the node with least time will always come first, so final ans will be minimum;
+        }
+        int sol=0;
+        unordered_set<int> hash; //keep track of visited nodes;
+        hash.insert(k);
+        while(pq.size()){
+            int time=pq.top().first, node=pq.top().second;
+            pq.pop();
+            if(hash.find(node)!=hash.end()){ //we've seen this node before;
+                continue;
+            }
+            hash.insert(node);
+            for(auto x: adj[node]){
+                if(hash.find(x.second)!=hash.end()) continue;  //if node's already there, then no pt in pushing it to the pq;
+                pq.push({x.first+time, x.second}); //update time;
+            }
+            sol=max(sol, time);
+            if(hash.size()==n){ //all nodes are covered;
+                return sol;
             }
         }
-    }
-    
-    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        // Build the adjacency list
-        for (vector<int> time : times) {
-            int source = time[0];
-            int dest = time[1];
-            int travelTime = time[2];
-            
-            adj[source].push_back({travelTime, dest});
-        }
-
-        vector<int> signalReceivedAt(n + 1, INT_MAX);
-        BFS(signalReceivedAt, k);
-        
-        int answer = INT_MIN;
-        for (int i = 1; i <= n; i++) {
-            answer = max(answer, signalReceivedAt[i]);
-        }
-        
-        // INT_MAX signifies atleat one node is unreachable
-        return answer == INT_MAX ? -1 : answer;
+        return -1;
     }
 };
