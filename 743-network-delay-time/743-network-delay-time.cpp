00da6060e148fdcbf1d2d23dc96019fe3f83dbf7
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-06-20 12:41:41 +0530

    Time: 182 ms (47.74%), Space: 44.8 MB (19.07%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -9,10 +9,10 @@ public:
         }
         priority_queue<pii, vector<pii>, greater<pii>> pq; //min time will be first;
         for(auto x: adj[k]){
-            pq.push(x);
+            pq.push(x); //this way the node with least time will always come first, so final ans will be minimum;
         }
         int sol=0;
-        unordered_set<int> hash;
+        unordered_set<int> hash; //keep track of visited nodes;
         hash.insert(k);
         while(pq.size()){
             int time=pq.top().first, node=pq.top().second;
@@ -22,11 +22,11 @@ public:
             }
             hash.insert(node);
             for(auto x: adj[node]){
-                if(hash.find(x.second)!=hash.end()) continue;
-                pq.push({x.first+time, x.second});
+                if(hash.find(x.second)!=hash.end()) continue;  //if node's already there, then no pt in pushing it to the pq;
+                pq.push({x.first+time, x.second}); //update time;
             }
             sol=max(sol, time);
-            if(hash.size()==n){
+            if(hash.size()==n){ //all nodes are covered;
                 return sol;
             }
         }
