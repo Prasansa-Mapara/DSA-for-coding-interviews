48adc95b0bb422f405318ee39686313b9431d4e9
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-06-22 02:20:01 +0530

    Time: 66 ms (93.82%), Space: 48.4 MB (77.57%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int n=nums.size();
-        if(n==1) return 1;
-        vector<bool> dp(n, 0);
-        dp[n-1]=1;
+        //just keep a track of which index's the safest, if i wanna reach the end;
+        int n=nums.size(), safe=n-1; //cuz last pos's the safest;
         for(int i=n-2; i>=0; i--){
-            if(dp[i]) continue;
-            int x=nums[i];
-            for(int j=0; j<=x; j++){
-                if(i+j<n) dp[i]=dp[i]||dp[i+j];
-                else break;
-                if(dp[i]) break;
+            int jump=i+nums[i]; //max pos i can reach from this index;
+            if(jump>=safe){ //if we can reach the safe index from here, then all we gotta do
+                //is try reach this index next, cuz it'll be the min index;
+                safe=i;
             }
         }
-        return dp[0];
+        return safe==0; //if it's safe to reach end from first pos;
     }
 };
