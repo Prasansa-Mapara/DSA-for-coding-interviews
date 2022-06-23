3effbd54ed0cc74e1cdd4e9076ed571ebbe1de8a
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-06-23 20:59:53 +0530

    Time: 15 ms (89.90%), Space: 17.1 MB (74.22%) - LeetHub

--- 57-insert-interval/57-insert-interval.cpp
+++ 57-insert-interval/57-insert-interval.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
-    vector<vector<int>> insert(vector<vector<int>> &v, vector<int>& curr) 
-    {
-        vector<vector<int>> sol;
+    vector<vector<int>> insert(vector<vector<int>>& v, vector<int>& curr) {
         int n=v.size();
+        vector<vector<int>> sol;
         for(int i=0; i<n; i++){
             if(v[i][1]<curr[0]){
                 sol.push_back(v[i]);
@@ -13,8 +12,8 @@ public:
                 curr=v[i];
             }
             else{
-                curr[0]=min(curr[0], v[i][0]);
-                curr[1]=max(curr[1], v[i][1]);
+                curr[0]=min(v[i][0], curr[0]);
+                curr[1]=max(v[i][1], curr[1]);
             }
         }
         sol.push_back(curr);
