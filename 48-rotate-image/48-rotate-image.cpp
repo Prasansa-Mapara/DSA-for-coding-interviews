e5010f50624d87826d55231455de08f998f23af0
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-06-24 17:03:44 +0530

    Time: 11 ms (8.87%), Space: 7 MB (73.17%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& mat) {
-        int n=mat.size();
         reverse(mat.begin(), mat.end());
+        int n=mat.size();
         for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(j>i){
-                    swap(mat[i][j], mat[j][i]);
-                }
+            for(int j=i+1; j<n; j++){
+                swap(mat[i][j], mat[j][i]);
             }
         }
         return;
