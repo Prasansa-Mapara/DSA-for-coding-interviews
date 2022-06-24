ade727cf015135c03c00198ab73fb2c154e1acc2
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-06-24 17:28:49 +0530

    Time: 32 ms (14.85%), Space: 13 MB (99.92%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -2,39 +2,44 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& mat) {
         int r=mat.size(), c=mat[0].size();
-        bool rr=1, cc=1;
+        bool r0=0, c0=0;
         for(int j=0; j<c; j++){
-            if(mat[0][j]==0){
-                rr=0;
+            if(!mat[0][j]){
+                r0=1;  //first row will be 0;
                 break;
             }
         }
         for(int i=0; i<r; i++){
-            if(mat[i][0]==0){
-                cc=0;
+            if(!mat[i][0]){
+                c0=1; //first col will be 0;
                 break;
             }
         }
         for(int i=1; i<r; i++){
             for(int j=1; j<c; j++){
-                if(mat[i][j]==0){
+                if(!mat[i][j]){
+                    mat[0][j]=0; //placeholders;
                     mat[i][0]=0;
-                    mat[0][j]=0;
                 }
             }
         }
         for(int i=1; i<r; i++){
             for(int j=1; j<c; j++){
-                if(mat[i][0]==0 || mat[0][j]==0){
-                    mat[i][j]=0;
+                if(!mat[i][0] || !mat[0][j]){
+                    mat[i][j]=0; //will be 0 if its corresponding row or col is 0;
                 }
             }
         }
-        if(rr==0){
-            for(int j=0; j<c; j++) mat[0][j]=0;
+        if(r0){
+            for(int j=0; j<c; j++){
+                mat[0][j]=0;
+            }
         }
-        if(cc==0){
-            for(int i=0; i<r; i++) mat[i][0]=0;
+        if(c0){
+            for(int i=0; i<r; i++){
+                mat[i][0]=0;
+            }
         }
+        return;
     }
 };
