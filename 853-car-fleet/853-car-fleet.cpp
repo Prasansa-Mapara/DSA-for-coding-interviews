baff67f61eb951fd0cfbcb166eabe6e28cdbc28f
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-06-25 16:42:04 +0530

    Time: 264 ms (70.92%), Space: 78.7 MB (57.27%) - LeetHub

--- 853-car-fleet/853-car-fleet.cpp
+++ 853-car-fleet/853-car-fleet.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
-     int carFleet(int k, vector<int>& pos, vector<int>& speed) {
-         //cars that reach the destination at the same time is a fleet;
-        //faster car will always catch up to the slower car;
-         //so prev car will catch up curr car, if time taken by prev car to reach the desti is less than
-         //time taken by curr car to reach the desti;
-         vector<pair<int, int>> v;
-         int n=pos.size();
-         for(int i=0; i<n; i++){
-             v.push_back({pos[i], speed[i]});
-         }
-         sort(v.begin(), v.end());
-         
-         stack<double> st;
-         for(int i=0; i<n; i++){
-             double curr=(double)(k-v[i].first)/(double)v[i].second; //time taken by curr car to reach desti;
-             while(st.size() && curr>=st.top()){ //if prev car reaches desti at same time or more time than curr car,
-                 //then they will end up meeting;
-                 st.pop();
-             }
-             st.push(curr);
-         }
-         return st.size();
+    int carFleet(int k, vector<int>& pos, vector<int>& speed) {
+        int n=pos.size();
+        vector<pair<int, int>> v;
+        for(int i=0; i<n; i++){
+            v.push_back({pos[i], speed[i]});
+        }
+        sort(v.begin(), v.end());
+        //if curr car takes equal or more time than prev car,
+        //then they will form a fleet;
+        stack<double> st;
+        for(int i=0; i<n; i++){
+            int pos=v[i].first, speed=v[i].second;
+            double curr=(double)(k-pos)/(double)speed;
+            while(st.size() && curr>=st.top()){
+                st.pop();
+            }
+            st.push(curr);
+        }
+        return st.size();
     }
 };
