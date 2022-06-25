d4e19821c09ec36bb41d7914b001b0d69f0883a9
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-06-25 15:20:03 +0530

    Time: 282 ms (31.42%), Space: 89 MB (28.51%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.cpp
+++ 739-daily-temperatures/739-daily-temperatures.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temp) {
+        //since we want to keep a decreasing order, can also try stack
+        //not using deque, cuz we'll never be accessing the front element;
+        stack<int> st;
         int n=temp.size();
         vector<int> sol(n, 0);
-        deque<int> dq;
-        //maintain a decreasing order;
         for(int i=0; i<n; i++){
-            while(dq.size() && temp[i]>temp[dq.back()]){
-                int prev=dq.back(); //curr temp is greater than prev temp;
-                dq.pop_back();
+            while(st.size() && temp[i]>temp[st.top()]){
+                int prev=st.top(); //cuz now the prev temp ghas found a hotter temp :)
+                st.pop();
                 sol[prev]=i-prev;
-            } 
-            dq.push_back(i);            
+            }
+            st.push(i);
         }
         return sol;
     }
