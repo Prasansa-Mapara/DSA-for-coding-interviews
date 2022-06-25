da9bf72db0f557995c2c36600edd3cbce2d3120d
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-06-25 16:27:04 +0530

    Time: 448 ms (19.37%), Space: 78.7 MB (57.27%) - LeetHub

--- 853-car-fleet/853-car-fleet.cpp
+++ 853-car-fleet/853-car-fleet.cpp
@@ -1,10 +1,24 @@
 class Solution {
 public:
-     int carFleet(int target, vector<int>& pos, vector<int>& speed) {
-        map<int, double> m;
-        for (int i = 0; i < pos.size(); i++) m[-pos[i]] = (double)(target - pos[i]) / speed[i];
-        int res = 0; double cur = 0;
-        for (auto it : m) if (it.second > cur) cur = it.second, res++;
-        return res;
+     int carFleet(int k, vector<int>& pos, vector<int>& speed) {
+         //cars that reach the destination at the same time is a fleet;
+        //faster car will always catch up to the slower car;
+         vector<pair<int, int>> v;
+         int n=pos.size();
+         for(int i=0; i<n; i++){
+             v.push_back({pos[i], speed[i]});
+         }
+         sort(v.begin(), v.end());
+         
+         stack<double> st;
+         for(int i=0; i<n; i++){
+             double curr=(double)(k-v[i].first)/(double)v[i].second; //time taken by curr car to reach desti;
+             while(st.size() && curr>=st.top()){ //if prev car reaches desti at same time or more time than curr car,
+                 //then they will end up meeting;
+                 st.pop();
+             }
+             st.push(curr);
+         }
+         return st.size();
     }
 };
