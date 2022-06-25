51f1dfb5e4f701c3e218cc74e18bc00e87d66aba
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-06-25 17:01:19 +0530

    Time: 273 ms (45.94%), Space: 79.6 MB (56.22%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -1,34 +1,21 @@
 class Solution {
 public:
-    int largestRectangleArea(vector<int>& heights) {
-        //if a smaller rectangle comes, then we'll be in trouble; 
-        //so we'll strictly maintain an increasing order;
-        
-        stack<pair<int, int>> st; //height->index;
-        //not storing indices directly cuz as we remove the larger height elements, we'll need to store that last index cuz it can go both left and right sides;
-        
-        int n=heights.size(), area=0;
+    int largestRectangleArea(vector<int>& height) {
+        stack<pair<int, int>> st;
+        int n=height.size(), sol=0;
         for(int i=0; i<n; i++){
-            if(st.empty() || heights[i]>=st.top().first){
-                st.push({heights[i], i});
+            int ind=i;
+            while(st.size() && height[i]<st.top().first){ //prev rectangle can't expand now;
+                ind=st.top().second;
+                sol=max(sol, st.top().first*(i-st.top().second));
+                st.pop();
             }
-            else{
-                int j;
-                while(st.size() && heights[i]<st.top().first){
-                    j=st.top().second; //curr element can go upto j in left side as its height is less than height of j;
-                    //we found the right index;
-                    area=max(area, st.top().first*(i-j));
-                    st.pop();
-                }
-                st.push({heights[i], j});
-            }
+            st.push({height[i], ind});
         }
-        
         while(st.size()){
-            area=max(area, st.top().first*(n-st.top().second));
+            sol=max(sol, st.top().first*(n-st.top().second));
             st.pop();
         }
-        
-        return area;
+        return sol;
     }
 };
