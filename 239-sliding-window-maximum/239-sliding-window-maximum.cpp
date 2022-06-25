166b66201e1eb9921fbd0eaa9050208f1c5b7773
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-06-25 15:07:13 +0530

    Time: 480 ms (41.26%), Space: 134.8 MB (38.78%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        int n=nums.size();
+        //we need to maintain elements in a descending order;
+        //so front of queue will always have max;
         deque<int> q;
-        for(int i=0; i<k-1; i++){
+        vector<int> sol;
+        int n=nums.size();
+        for(int i=0; i<k; i++){
             while(q.size() && nums[i]>nums[q.back()]){
                 q.pop_back();
             }
             q.push_back(i);
         }
-        vector<int> sol;
-        for(int i=k-1; i<n; i++){
+        sol.push_back(nums[q.front()]);
+        for(int i=k; i<n; i++){
             while(q.size() && nums[i]>nums[q.back()]){
                 q.pop_back();
             }
             q.push_back(i);
-            if((i-q.front()==k)) q.pop_front(); //size of sliding window is k;
+            if(i-q.front()+1>k) q.pop_front();
             sol.push_back(nums[q.front()]);
         }
         return sol;
