d318753fcf42943439a790bba2eb6a2ce1ef24e1
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-06-25 12:24:12 +0530

    Time: 23 ms (50.90%), Space: 19.7 MB (58.23%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int n=height.size(), l=0, r=n-1, lMx=0, rMx=0, sol=0;
-        while(l<r){ 
-            if(height[l]<=height[r]){ //there's already a wall on right side, so lMx will
-                //obviousy be the min (among lMx, and rMx), so for trapped water we'll 
-                //only consider lMx;
-                if(height[l]>=lMx){ //there's no wall on left side;
-                    lMx=height[l];
-                }
-                else{ //we've a higher wall on the left side;
-                    sol+=lMx-height[l];
-                }
+        //min(lMax, rMax)-height[i];
+        int lMax=0, rMax=0, l=0, r=height.size()-1, sol=0;
+        while(l<r){
+            if(height[l]>=lMax){
+                lMax=height[l];
+            }
+            if(height[r]>=rMax){
+                rMax=height[r];
+            }
+            int curr=min(height[l], height[r]);
+            sol+=min(lMax, rMax)-curr;
+            if(lMax<=rMax){
                 l++;
             }
-            else{ //there's already a wall on left side;
-                if(height[r]>=rMx){ //no wall on right side;
-                    rMx=height[r];
-                }
-                else{
-                    sol+=rMx-height[r]; //there's a higher wall on right side so water can be trapped;
-                }
+            else{
                 r--;
             }
         }
