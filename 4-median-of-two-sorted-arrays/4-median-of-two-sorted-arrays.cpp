7972da3508c0d8aed9bbe3f0bf5a4301e9748e56
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-06-27 12:13:42 +0530

    Time: 39 ms (91.03%), Space: 89.4 MB (61.26%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,38 +1,34 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int m=nums1.size(), n=nums2.size();
-        if(m>n){
-            swap(nums1, nums2);
-            swap(m, n);
-        }
-        int lo=0, hi=m;
-        
+        int n1=nums1.size(), n2=nums2.size();
+        if(n2<n1) return findMedianSortedArrays(nums2, nums1);
+        int lo=0, hi=n1;
+        int n1l, n1r, n2l, n2r;
         while(lo<=hi){
-            int mid=lo+(hi-lo)/2, rem=((m+n+1)/2)-mid;
-            int aL, aR, bL, bR;
-            aL=(mid>0)?(nums1[mid-1]):(INT_MIN);
-            aR=(mid<m)?(nums1[mid]):(INT_MAX);
-            bL=(rem>0)?(nums2[rem-1]):(INT_MIN);
-            bR=(rem<n)?(nums2[rem]):(INT_MAX);
+            int mid=lo+(hi-lo)/2;
+            int rem=(n1+n2+1)/2-mid;
+            n1l=(mid>0?nums1[mid-1]:INT_MIN);
+            n1r=(mid<n1?nums1[mid]:INT_MAX);
+            n2l=(rem>0?nums2[rem-1]:INT_MIN);
+            n2r=(rem<n2?nums2[rem]:INT_MAX);
             
-            if(aL<=bR && bL<=aR){
-                int tot=m+n;
-                if(tot%2){
-                    return (double)max(aL, bL);
+            if(n1l<=n2r && n1r>=n2l){
+                int n=n1+n2;
+                if(n%2){
+                    return (double)max(n1l, n2l);
                 }
                 else{
-                    return (double)(max(aL, bL)+min(aR, bR))/2.0;
+                    return (double)(max(n1l, n2l)+min(n1r, n2r))/2.0;
                 }
             }
-            else if(aL>bR){
+            else if(n1l>n2r){
                 hi=mid-1;
             }
             else{
                 lo=mid+1;
             }
         }
-        
         return 0;
     }
 };
