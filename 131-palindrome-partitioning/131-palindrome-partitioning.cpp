a51f6b7f2d9382e4a10389ad017a244f338af3ca
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-06-30 23:16:03 +0530

    Time: 256 ms (33.36%), Space: 163 MB (14.64%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
     vector<vector<string>> sol;
+    vector<vector<bool>> dp;
     
-    void solve(string &s, int st, vector<string> tmp, vector<vector<bool>> &dp){
+    void solve(string &s, int st, vector<string> curr){
         int n=s.size();
         if(st>=n){
-            sol.push_back(tmp);
+            sol.push_back(curr);
             return;
         }
-        for(int ed=st; ed<n; ed++){
-            string curr=s.substr(st, ed-st+1);
-            if(s[st]==s[ed] && ((ed-st)<=2 || dp[st+1][ed-1])){
-                tmp.push_back(curr);
-                dp[st][ed]=1;
-                solve(s, ed+1, tmp, dp);
-                tmp.pop_back();
+        for(int j=st; j<n; j++){
+            if(s[st]==s[j]){
+                if((j-st>=2 && dp[st+1][j-1]) || j-st<2){
+                    dp[st][j]=1;
+                    curr.push_back(s.substr(st, j-st+1));
+                    solve(s, j+1, curr);
+                    curr.pop_back();
+                }
             }
         }
-        return;
     }
     
     vector<vector<string>> partition(string s) {
+        vector<string> curr;
         int n=s.size();
-        vector<vector<bool>> dp(n, vector<bool>(n, 0));
-        vector<string> tmp;
-        solve(s, 0, tmp, dp);
+        vector<vector<bool>> tmp(n, vector<bool>(n, 0));
+        dp=tmp;
+        solve(s, 0, curr); 
         return sol;
     }
 };
