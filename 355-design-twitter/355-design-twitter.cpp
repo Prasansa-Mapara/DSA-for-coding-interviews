201e87fc3e4f07e9364168924841251c7c316a91
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-06-30 20:23:01 +0530

    Time: 7 ms (16.98%), Space: 6.9 MB (99.15%) - LeetHub

--- 355-design-twitter/355-design-twitter.cpp
+++ 355-design-twitter/355-design-twitter.cpp
@@ -2,46 +2,43 @@
 
 class Twitter {
 public:
-    unordered_map<int, unordered_set<int>> users;
-    vector<pii> tweets;
+    unordered_map<int, unordered_set<int>> hash;
+    vector<pii> v;
     
     Twitter() {
         
     }
     
     void postTweet(int uid, int tid) {
-        tweets.push_back({uid, tid});
+        v.push_back({uid, tid});
     }
     
     vector<int> getNewsFeed(int uid) {
-        //we want 10 recent tweets;
-        //these tweets should either have been tweeted by the user, or
-        //by the users followed by our curr user;
-        //which is simple, as our array tweets, is alreayd sorted;
-        
-        int n=tweets.size();
+        //the most recent tweet's in the end;
+        int n=v.size();
         vector<int> sol;
-        for(int i=n-1; i>=0; i--){ //iterating from the most recent tweet;
-             auto curr=tweets[i];
-            //if curr user is the given user or given user follows curr user;
-            if(uid==curr.first || users[uid].find(curr.first)!=users[uid].end()){
-                sol.push_back(curr.second);
+        for(int i=n-1; i>=0; i--){
+            int curUser=v[i].first, curTweet=v[i].second;
+            //curUser either has to be same as the given user,
+            //or it should be one of the users followed by the given user;
+            if(curUser==uid || hash[uid].find(curUser)!=hash[uid].end()){
+                sol.push_back(curTweet);
                 if(sol.size()==10){
                     return sol;
                 }
             }
         }
-        
+        //if num of tweets are less than 10;
         return sol;
     }
     
-    void follow(int fid, int feid) {
-        users[fid].insert(feid);
+    void follow(int fr, int fe) {
+        hash[fr].insert(fe);
     }
     
-    void unfollow(int fid, int feid) {
-        if(users[fid].find(feid)!=users[fid].end()){
-            users[fid].erase(feid);
+    void unfollow(int fr, int fe) {
+        if(hash[fr].find(fe)!=hash[fr].end()){
+            hash[fr].erase(fe);
         }
     }
 };
