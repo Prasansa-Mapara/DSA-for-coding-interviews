e37f39102face520002f1b1d7d44bb0b0199bb1c
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-07-02 21:23:57 +0530

    Time: 42 ms (44.30%), Space: 8.9 MB (86.05%) - LeetHub

--- 778-swim-in-rising-water/778-swim-in-rising-water.cpp
+++ 778-swim-in-rising-water/778-swim-in-rising-water.cpp
@@ -1,34 +1,40 @@
-#define pii pair<int, int>
+#define ppi pair<int, pair<int, int>>
+#define ff first
+#define ss second
 
 class Solution {
 public:
     int swimInWater(vector<vector<int>>& mat) {
         int r=mat.size(), c=mat[0].size();
-        //here, since we know that we can reach the last cell in the time (*max_element(mat.begin(), mat.end()));
-        //so, one way is to do binary search, while doing bfs traversal ofc;
-        //with that approach, tc will be O(logn*n^2), n^2 fro bfs or dfs;
-         
-        //another way is to do pq+bfs;
-        priority_queue<pair<int, pii>, vector<pair<int, pii>>, greater<pair<int, pii>>> pq;
+        //we want least time;
+        //can travel from cell x, to cell y at time max(mat[x], mat[y]);
+        //since we want min time, we can maintain a min heap, and traverse in a bfs manner;
+        //pq will have {time, {x, y}};
+        
+        priority_queue<ppi, vector<ppi>, greater<ppi>> pq;
         pq.push({mat[0][0], {0, 0}});
+        vector<vector<int>> dir={{0,1},{1,0},{-1,0},{0,-1}};
+        
         vector<vector<bool>> vis(r, vector<bool>(c, 0));
         vis[0][0]=1;
-        vector<vector<int>> dir={{0,1}, {0,-1}, {1,0}, {-1,0}};
         
         while(pq.size()){
-            int time=pq.top().first, x=pq.top().second.first, y=pq.top().second.second;
-            if(x==(r-1) && y==(c-1)) return time;
+            int time=pq.top().ff, x=pq.top().ss.ff, y=pq.top().ss.ss;
             pq.pop();
-            for(int i=0; i<4; i++){
+            if(x==r-1 && y==c-1) return time; //cuz it will be the min time, 
+            //since we've a min heap;
+            for(int i=0; i<4; i++){ //bfs traversal;
                 int xx=x+dir[i][0], yy=y+dir[i][1];
-                if(xx>=0 && xx<r && yy>=0 && yy<c && !vis[xx][yy]){
+                if(xx>=0 && yy>=0 && xx<r && yy<c && !vis[xx][yy]){ //if we've seen this cell before, then
+                    //at that time, time would've been minimum, since we are always taking nodes that have 
+                    //minimum time; so no need to store this one;
+                    vis[xx][yy]=1;
                     if(xx==r-1 && yy==c-1){
-                        return max(time, mat[xx][yy]);
+                        return max(time, mat[xx][yy]); //as we can swim at time that's max of both;
                     }
                     pq.push({max(time, mat[xx][yy]), {xx, yy}});
-                    vis[xx][yy]=1;
                 }
-            }
+            }          
         }
         
         return 0;
