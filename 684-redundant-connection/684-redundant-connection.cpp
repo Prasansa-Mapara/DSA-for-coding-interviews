4f77377124cbb4e77d5e303f4dbe8805e40994ea
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-07-02 16:47:02 +0530

    Time: 68 ms (5.19%), Space: 79.7 MB (5.02%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.cpp
+++ 684-redundant-connection/684-redundant-connection.cpp
@@ -1,39 +1,30 @@
 class Solution {
 public:
-    
-    int find(int curr, vector<int> root){
-        if(curr==root[curr]){
-            return curr;
+    int findRoot(int i, vector<int> root){
+        if(i==root[i]){
+            return i;
         }
-        root[curr]=find(root[curr], root); //cuz we want the main root; 
-        //so will assign that main root to every node in btwn,
-        //so in future, we can get it in O(1);
-        return root[curr]; 
+        return root[i]=findRoot(root[i], root);
     }
     
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        //this is plain application of union find;
-        //itearte through the edges, unionize those vertices,
-        //if those vertices are already in the same set, then return ans;
-        //here, simple dfs could also have done, but we want last edge, so union find's better way to do it;
-        
         int n=edges.size();
         vector<int> root(n+1, 0), rank(n+1, 1);
         for(int i=1; i<=n; i++){
-            root[i]=i;
+            root[i]=i; //initially each set's disjoint;
         }
         for(int i=0; i<n; i++){
             int x=edges[i][0], y=edges[i][1];
-            int rootX=find(x, root), rootY=find(y, root);
-            if(rootX==rootY){ //they alreayd connected;
+            int rootX=findRoot(x, root), rootY=findRoot(y, root);
+            if(rootX==rootY){
                 return {x, y};
             }
-            if(rank[rootX]>rank[rootY]){
+            else if(rank[rootX]>rank[rootY]){ //rootX has more power;
                 rank[rootX]+=rank[rootY];
                 root[rootY]=rootX;
             }
-            else{
-                rank[rootY]+=rank[rootX];
+            else{ //rootY has more power
+                rank[rootY]+=rootX;
                 root[rootX]=rootY;
             }
         }
