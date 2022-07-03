f691ad15ea36bdc27c889b77e0256d8a9b5912fd
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-07-03 18:58:01 +0530

    Time: 10 ms (63.99%), Space: 13.7 MB (71.77%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -3,17 +3,9 @@ public:
     int maxProduct(vector<int>& nums) {
         int curMx=1, curMn=1, sol=INT_MIN;
         for(auto n: nums){
-            if(n==0){
-                curMx=1;
-                curMn=1;
-                sol=max(sol, 0);
-                continue;
-            }
-            else{
-                int x=n*curMx, y=n*curMn;
-                curMx=max(x, max(y, n)); 
-                curMn=min(x, min(y, n)); 
-            }
+            int x=n*curMx, y=n*curMn;
+            curMx=max(x, max(y, n)); 
+            curMn=min(x, min(y, n)); 
             sol=max(sol, curMx); 
         }
         return sol;
