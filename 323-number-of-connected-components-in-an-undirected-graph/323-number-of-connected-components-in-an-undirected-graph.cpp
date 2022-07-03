7a9624ce5593fe55dc0c98479b44e56046720a33
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-07-03 15:12:03 +0530

    Time: 44 ms (23.90%), Space: 12.8 MB (58.13%) - LeetHub

--- 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.cpp
+++ 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.cpp
@@ -2,41 +2,36 @@ class Solution {
 public:
     vector<int> root;
     
-    int find(int i){
-        if(i==root[i]){
-            return i;
+    int find(int x){
+        if(x==root[x]){
+            return x;
         }
-        return root[i]=find(root[i]);
+        return root[x]=find(root[x]);
     }
     
     int countComponents(int n, vector<vector<int>>& edges) {
-        //do union find, then check for number of distinct roots;
-        //tc: O(N);
         vector<int> rank(n, 1);
-        root.resize(n);
+        root=rank;
         for(int i=0; i<n; i++){
             root[i]=i;
         }
-        int sz=edges.size(), sol=n;
-        for(int i=0; i<sz; i++){
-            int x=edges[i][0], y=edges[i][1];
-            int rootX=find(x), rootY=find(y);
-            if(rootX!=rootY){ //if these vertices aren't already connected;
-                if(rank[rootX]>rank[rootY]){
-                    rank[rootX]+=rank[rootY];
-                    root[rootY]=rootX;
-                }
-                else{
-                    rank[rootY]+=rank[rootX];
-                    root[rootX]=rootY;
-                }
-                sol--; //since i have connected x and y by an edges now, 
-                //number of connected components will reduce by 1;
-                //ex: 1,2,3 -> conn=3
-                //1-2, 3 -> conn=2;
+        int sol=n;
+        for(auto e: edges){
+            int rx=find(e[0]), ry=find(e[1]);
+            if(rx==ry) continue;
+            
+            if(rank[rx]>rank[ry]){
+                root[ry]=rx;
             }
+            else if(rank[rx]<rank[ry]){
+                root[rx]=ry;
+            }
+            else{
+                root[ry]=rx;
+                rank[rx]++;
+            }
+            sol--;
         }
-        
         return sol;
     }
 };
