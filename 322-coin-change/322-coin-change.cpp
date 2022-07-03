d5b13df35cfd6600a053e24484d086524d80a708
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-07-03 18:06:40 +0530

    Time: 113 ms (63.93%), Space: 14.5 MB (57.69%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     int coinChange(vector<int>& cns, int amt) {
         int n=cns.size();
-        vector<int> dp(amt+1, INT_MAX);
+        vector<int> dp(amt+1, amt+1);
         dp[0]=0; 
         sort(cns.begin(), cns.end());
         for(int i=1; i<=amt; i++){
             for(int j=0; j<n; j++){
                 if(cns[j]>i) break;
-                if(dp[i-cns[j]]!=INT_MAX)dp[i]=min(dp[i], 1+dp[i-cns[j]]);
+                dp[i]=min(dp[i], 1+dp[i-cns[j]]);
             }
         }
-        return dp[amt]==INT_MAX?-1:dp[amt];
+        return dp[amt]==amt+1?-1:dp[amt];
     }
 };
