268f00ac9e7715afde6eb65dd86ee729c9081e5a
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-07-03 18:05:21 +0530

    Time: 218 ms (16.91%), Space: 14.4 MB (68.70%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,25 +1,16 @@
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int amt) {
-        //dp[amt]=min(dp[amt], 1+dp[amt-cns[i]]);
-        //so we need to follow bottom-up approach;
-        //initially we'll fill up the dp table with max value, which will be amt+1 in this case
-        //as we will never need more than amt number of coins to make up amt;
-        //we also know dp[0]=0;
-        
-        vector<int> dp(amt+1, amt+1);
-        dp[0]=0;
-        sort(coins.begin(), coins.end());
+    int coinChange(vector<int>& cns, int amt) {
+        int n=cns.size();
+        vector<int> dp(amt+1, INT_MAX);
+        dp[0]=0; 
+        sort(cns.begin(), cns.end());
         for(int i=1; i<=amt; i++){
-            for(auto currC: coins){
-                if(currC<=i){ //because the amt is i, so coin will have to be less than or equal to i;
-                    dp[i]=min(dp[i], 1+dp[i-currC]);
-                }
-                else{
-                    break; //cuz all other coins will be greater than i, as coins array is sorted;
-                }
+            for(int j=0; j<n; j++){
+                if(cns[j]>i) break;
+                if(dp[i-cns[j]]!=INT_MAX)dp[i]=min(dp[i], 1+dp[i-cns[j]]);
             }
         }
-        return dp[amt]==(amt+1)?-1:dp[amt];
+        return dp[amt]==INT_MAX?-1:dp[amt];
     }
 };
