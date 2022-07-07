061c437aa21ab735426d6ae8fbabae80fd1401bd
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-07-08 01:47:21 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (61.15%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<int> dp(n, 1);
+        vector<int> prev(n, 1), curr=prev;
         for(int i=1; i<m; i++){
+            curr=prev;
             for(int j=1; j<n; j++){
-                dp[j]+=dp[j-1];
+                curr[j]=prev[j]+curr[j-1];
             }
+            prev=curr;
         }
-        return dp[n-1];
+        return curr[n-1];
     }
 };
