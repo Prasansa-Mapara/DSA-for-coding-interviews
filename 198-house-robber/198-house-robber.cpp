b4c698dc6eec09def8391ded545eea02a4fcbe47
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-07-08 01:23:31 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (47.19%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        //i can either rob the current house, or not;
         int n=nums.size();
-        if(n>1) nums[1]=max(nums[1], nums[0]);
-        for(int i=2; i<n; i++){
-            nums[i]=max(nums[i]+nums[i-2], nums[i-1]);
+        vector<int> dp(n+2, 0);
+        for(int i=0; i<n; i++){
+            dp[i+2]=max(nums[i]+dp[i], dp[i+1]);
         }
-        return nums[n-1];
+        return dp[n+1];
     }
 };
