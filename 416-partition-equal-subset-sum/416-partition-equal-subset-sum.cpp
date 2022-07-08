d3aaa8219b21125a90a557624ef29f14e0c08f53
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-07-08 13:25:37 +0530

    Time: 468 ms (25.69%), Space: 9.2 MB (89.01%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -7,17 +7,17 @@ public:
         }
         if(sum%2) return 0;
         int target=sum/2;
-        vector<vector<bool>> dp(n, vector<bool>(target+1, 0));
-        dp[0][0]=1;
-        if(nums[0]<=target) dp[0][nums[0]]=1;
+        vector<bool> dp(target+1, 0), curr=dp;
+        dp[0]=1; if(nums[0]<=target) dp[nums[0]]=1;
+        
         for(int i=1; i<n; i++){
             for(int k=0; k<=target; k++){
-                bool take=0, notTake=0;
-                if(k>=nums[i])take|= dp[i-1][k-nums[i]];
-                notTake|= dp[i-1][k];
-                dp[i][k]= take| notTake;
+                curr[k]=0;
+                if(k>=nums[i])curr[k]=dp[k-nums[i]];
+                curr[k]=curr[k] | dp[k];
             }
+            dp=curr;
         }
-        return dp[n-1][target];
+        return dp[target];
     }
 };
