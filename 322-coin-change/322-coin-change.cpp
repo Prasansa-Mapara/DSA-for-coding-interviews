e489cc02db851ba1741982908776e70025f33ea3
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-07-09 21:31:55 +0530

    Time: 208 ms (19.36%), Space: 14.5 MB (58.62%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
-    int coinChange(vector<int>& cns, int amt) {
-        int n=cns.size();
-        vector<int> dp(amt+1, amt+1);
-        dp[0]=0; 
-        sort(cns.begin(), cns.end());
-        for(int i=1; i<=amt; i++){
-            for(int j=0; j<n; j++){
-                if(cns[j]>i) break;
-                dp[i]=min(dp[i], 1+dp[i-cns[j]]);
+    int coinChange(vector<int>& coins, int amount) {
+        //dp[i][j]=min(dp[i][j], 1+dp[i][j-coins[i]]);
+        int n=coins.size();
+        //since we've an infinite reserve of coins, there's no reason of 
+        //keeping track about which coin i'm on;
+        
+        sort(coins.begin(), coins.end());
+        
+        vector<int> dp(amount+1, amount+1);
+        dp[0]=0; //to make amount 0, i need 0 coins;
+        
+        for(int i=1; i<=amount; i++){ 
+            for(int j=0; j<n; j++){ //ith amount using jth coin;
+                if(coins[j]>i) break;
+                dp[i]=min(dp[i], 1+dp[i-coins[j]]);
             }
         }
-        return dp[amt]==amt+1?-1:dp[amt];
+        
+        return dp[amount]>amount?-1: dp[amount];
     }
 };
