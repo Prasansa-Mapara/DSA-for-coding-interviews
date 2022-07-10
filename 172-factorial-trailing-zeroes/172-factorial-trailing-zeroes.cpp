90e2e40a09014591bbb1bb2a72aecd0e024d2243
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-07-10 16:30:04 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (5.84%) - LeetHub

--- 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.cpp
+++ 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     int trailingZeroes(int n) {
-        //zeroes is a result of 2*5, 
-        //2 to bhar bharke honge, so we need total number of fives;
-        //25! -> 25*..*20*..*15*..*10*..*5*..*1
-        
-        int ans=0;
+        //these are cuz of 2*5, since we'll always have more 2's than 5's
+        //we'll count the total occurence of 5's
+        int sol=0;
         for(int i=1; pow(5, i)<=n; i++){
-            ans+=n/pow(5, i);
+            sol+=n/(pow(5, i));
         }
-        return ans;
+        return sol;
     }
 };
