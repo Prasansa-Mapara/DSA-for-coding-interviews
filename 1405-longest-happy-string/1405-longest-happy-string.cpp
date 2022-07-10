8d908ba03fa313203cfb37dac38d0021c63b1231
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-07-10 17:35:52 +0530

    Time: 4 ms (28.80%), Space: 5.9 MB (98.58%) - LeetHub

--- 1405-longest-happy-string/1405-longest-happy-string.cpp
+++ 1405-longest-happy-string/1405-longest-happy-string.cpp
@@ -1,30 +1,64 @@
 class Solution {
 public:
     string longestDiverseString(int a, int b, int c) {
-        int total=a+b+c;
-        int currA=0, currB=0, currC=0;
-        string res = "";
-        while(total--){
-            
-            if((a>=b && a>=c && currA!=2) || (a>0 && (currB==2|| currC==2))){
-                res+='a'; 
-                a--;
-                currA++;
-                currB=currC=0;
+        int n=a+b+c, cntA=0, cntB=0, cntC=0;
+        string s;
+        while(n--){
+            if(a>=b && a>=c && cntA<2){
+                s.push_back('a');
+                a--; cntA++;
+                cntB=0; cntC=0;
             }
-            else if((b>=a && b>=c && currB!=2) || (b>0 && (currA==2|| currC==2))){
-                res+='b';
-                b--;
-                currB++;
-                currA=currC=0;
+            else if(b>=c && b>=a && cntB<2){
+                s.push_back('b');
+                b--; cntB++;
+                cntA=0; cntC=0;
             }
-            else if((c>=a && c>=b && currC!=2) || (c>0 && (currA==2|| currB==2))){
-                res+='c';
-                c--;
-                currC++;
-                currA=currB=0;
+            else if(c>=a && c>=b && cntC<2){
+                s.push_back('c');
+                c--; cntC++;
+                cntA=0; cntB=0;
+            }
+            else{
+                if(cntA==2){
+                    if(cntB!=2 && b && b>=c){
+                        s.push_back('b');
+                        b--;
+                        cntA=0; cntB++;
+                    }
+                    else if(cntC!=2 && c && c>=b){
+                        s.push_back('c');
+                        c--;
+                        cntA=0; cntC++;
+                    }
+                }
+                else if(cntB==2){
+                    if(cntA!=2 && a && a>=c){
+                        s.push_back('a');
+                        a--;
+                        cntB=0; cntA++;
+                    }
+                    else if(cntC!=2 && c && c>=a){
+                        s.push_back('c');
+                        c--;
+                        cntB=0; cntC++;
+                    }
+                }
+                else if(cntC==2){
+                    if(cntB!=2 && b && b>=a){
+                        s.push_back('b');
+                        b--;
+                        cntC=0; cntB++;
+                    }
+                    else if(cntA!=2 && a && a>=b){
+                        s.push_back('a');
+                        a--;
+                        cntC=0; cntA++;
+                    }
+                }
             }
         }
-        return res;
+        
+        return s;
     }
 };
