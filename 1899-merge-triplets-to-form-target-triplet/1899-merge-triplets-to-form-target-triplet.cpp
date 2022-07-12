23089aa4a413cf56235253d469edd4c50fdac44d
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-07-12 22:56:57 +0530

    Time: 1015 ms (18.35%), Space: 163.8 MB (12.88%) - LeetHub

--- 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.cpp
+++ 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.cpp
@@ -3,12 +3,13 @@ public:
     bool mergeTriplets(vector<vector<int>>& triplets, vector<int>& target) {
         vector<int> v(3, 0);
         for(auto i: triplets){
-            if(i[0]<=target[0] && i[1]<=target[1] && i[2]<=target[2]){
+            if(i[0]<=target[0] && i[1]<=target[1] && i[2]<=target[2]){ //if we can do the op on this triplet;
+                //then do the op;
                 v[0]=max(v[0], i[0]);
                 v[1]=max(v[1], i[1]);
                 v[2]=max(v[2], i[2]);
             }
         }
-        return v==target;
+        return v==target; //after doing all possible ops;
     }
 };
