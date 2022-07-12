e89c75e715877e3e41f909290ccff7d564da1614
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-07-12 14:00:43 +0530

    Time: 69 ms (36.28%), Space: 30.1 MB (26.77%) - LeetHub

---
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -0,0 +1,66 @@
+/*#define ll long long
+
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int ns=s.size(), nt=t.size();
+        if(ns<nt) return 0;
+        if(ns==nt){
+            if(s==t) return 1;
+            return 0;
+        }
+        
+        //i can't skip any chars from t;
+        //since we want the total number of ways, we'll add both;
+        //if(s[i-1]==t[j-1]) dp[i][j]=dp[i-1][j-1] + dp[i-1][j]; 
+        //above approach is number of ways i select this char+ number of ways i dont select this char;
+        //else dp[i][j]=dp[i-1][j]; //if they dont match, then i cant select this char;
+        
+        vector<vector<ll>> dp(ns+1, vector<ll>(nt+1, 0));
+        
+        for(int i=0; i<=ns; i++){ //i dont take any chars from t;
+            dp[i][0]=1; //so total number of ways is 1(don't take this char);
+        }
+        
+        for(int i=1; i<=ns; i++){
+            for(int j=1; j<=nt; j++){
+                if(s[i-1]==t[j-1]){
+                    dp[i][j]+=dp[i-1][j-1];
+                }
+                dp[i][j]+=dp[i-1][j];
+            }
+        }
+        
+        return dp[ns][nt];
+    }
+};*/
+
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int m=s.length(),n=t.length();
+        vector<vector<unsigned long>> dp(n+1,vector<unsigned long>(m+1,0));
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=m;j++){
+                if(i==0 && j==0){
+                    dp[i][j]=1;
+                }
+                else if(i==0){
+                    dp[i][j]=dp[i][j-1];
+                }
+                else if(j==0){
+                    dp[i][j]=0;
+                }
+                else{
+                    if(s[j-1]==t[i-1]){
+                        dp[i][j]=dp[i-1][j-1]+dp[i][j-1];
+                    }
+                    else{
+                        dp[i][j]=dp[i][j-1];
+                    }
+                    
+                }
+            }
+        }return dp[n][m];
+    }
+};
