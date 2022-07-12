49dbe0d6e27deb36d70fd304574c067ede4f21b0
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-07-12 16:52:29 +0530

    Time: 10 ms (52.98%), Space: 6.9 MB (62.87%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.cpp
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -2,29 +2,33 @@ class Solution {
 public:
     bool isMatch(string s, string p) {
         int ns=s.size(), np=p.size();
-        vector<vector<bool>> dp(np+1, vector<bool>(ns+1, 0));
+        //* means p's preceding char can be repeated 0 or more times;
+        vector<vector<bool>> dp(ns+1, vector<bool>(np+1, 0));
         dp[0][0]=1;
-        
-        for(int i=2; i<=np; i++){
-            if(p[i-1]=='*'){
-                dp[i][0]=dp[i-2][0];
+        for(int j=2; j<=np; j++){
+            if(p[j-1]=='*'){
+                dp[0][j]=dp[0][j-2];
             }
         }
-        
-        for(int i=1; i<=np; i++){
-            for(int j=1; j<=ns; j++){
-                if(p[i-1]==s[j-1] || p[i-1]=='.'){
+        //if(np>=2 && p[1]=='*') dp[0][2]=1;
+        for(int i=1; i<=ns; i++){
+            for(int j=1; j<=np; j++){
+                if(s[i-1]==p[j-1] || p[j-1]=='.'){
                     dp[i][j]=dp[i-1][j-1];
                 }
-                else if(p[i-1]=='*'){
-                    dp[i][j]=dp[i-2][j];
-                    if(p[i-2]=='.' || p[i-2]==s[j-1]){
-                        dp[i][j]=dp[i][j-1] || dp[i][j];
+                else if(p[j-1]=='*'){
+                    if(s[i-1]==p[j-2] || p[j-2]=='.'){
+                        dp[i][j]=dp[i-1][j]; //if i wish to take star in future;
+                        dp[i][j]=dp[i-1][j-2] || dp[i][j]; //if i dont wish to take star in future;
+                        dp[i][j]=dp[i][j-2] || dp[i][j]; //neither take the star, nor match with the string;
+                    }
+                    else{
+                        dp[i][j]=dp[i][j-2]; //since doesn't match, can't take the star;
                     }
                 }
             }
         }
         
-        return dp[np][ns];
+        return dp[ns][np];
     }
 };
