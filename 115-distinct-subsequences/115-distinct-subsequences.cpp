87ea5390aae1e49a09188e4cdf7a54845670be3a
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-07-12 14:01:43 +0530

    Time: 25 ms (92.12%), Space: 18.8 MB (34.76%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -1,4 +1,4 @@
-/*#define ll long long
+#define ll unsigned long
 
 class Solution {
 public:
@@ -32,35 +32,5 @@ public:
         }
         
         return dp[ns][nt];
-    }
-};*/
-
-class Solution {
-public:
-    int numDistinct(string s, string t) {
-        int m=s.length(),n=t.length();
-        vector<vector<unsigned long>> dp(n+1,vector<unsigned long>(m+1,0));
-        for(int i=0;i<=n;i++){
-            for(int j=0;j<=m;j++){
-                if(i==0 && j==0){
-                    dp[i][j]=1;
-                }
-                else if(i==0){
-                    dp[i][j]=dp[i][j-1];
-                }
-                else if(j==0){
-                    dp[i][j]=0;
-                }
-                else{
-                    if(s[j-1]==t[i-1]){
-                        dp[i][j]=dp[i-1][j-1]+dp[i][j-1];
-                    }
-                    else{
-                        dp[i][j]=dp[i][j-1];
-                    }
-                    
-                }
-            }
-        }return dp[n][m];
     }
 };
