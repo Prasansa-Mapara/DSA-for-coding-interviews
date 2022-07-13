e999b85ab46c2bf77910aac242caa317b45986dc
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-07-13 13:54:17 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (79.99%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
-    
     double myPow(double x, int n) {
-        long long N=n;
-        if(N<0){
-            x=1/x;
-            N=-N;
+        if(n==0 ) return (double) 1;
+        else if(n==1) return x;
+        else if(n==-1) return 1/x;
+        double a=myPow(x, n/2), sol=1;
+        if(n>0 && n%2){
+            sol=x;
         }
-        if(N==0) return (double)1;
-        else if(N==1) return x;
-        double y=myPow(x, N/2), ans=1;
-        if(N%2){
-            ans*=x;
+        else if(n<0 && abs(n)%2){
+            sol=1/x;
         }
-        ans*=y*y;
-        return ans;
+        sol*=a*a;
+        return sol;
     }
 };
