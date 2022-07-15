969aa7a23503012b1b84e873c7db5d1b6a0b8a61
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-07-15 15:46:36 +0530

    Time: 25 ms (37.64%), Space: 7.1 MB (52.71%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int l=0, r, n=s.size(), cnt=0, sol=0;
+        int n=s.size();
+        if(n<=1) return n;
+        //we just need a sliding window;
+        //we'll keep max freq of occurence, subtract it from len of sliding window;
+        //if that value is less than or equal to k, we'll update our sol;
+        //else, we'll just increment our left ptr;
+        //here, we only have to update our freq when its greater than curr, cuz
+        //otherwise, solution's anyway not gonna work;
+        int l=0, r=0, sol=1;
         unordered_map<char, int> hash;
-        
-        for(int r=0; r<n; r++){
-            hash[s[r]]++; //update frequency of curr element; 
-            cnt=max(cnt, hash[s[r]]); //cnt will be the max frequency of an element in this window; 
-            //we are not updating cnt with every iteration, cuz for sol to change, cnt will have to be increased;
-            //so in case cnt remains the same, sol will also remain the same; 
-            int x=(r-l+1)-cnt; //num of chars to replace in this window will be total window size- number of characters that appear most frequently; 
-            if(x>k){
-                hash[s[l]]--;
-                l++; //length will have to decrease, so sol will remain same; 
+        int cnt=0; //max freq;
+        for(r=0; r<n; r++){
+            hash[s[r]]++;
+            cnt=max(cnt, hash[s[r]]);
+            int change=r-l+1-cnt;
+            if(change<=k){
+                sol=max(sol, r-l+1);
             }
-            sol=max(sol, r-l+1); //it is easy to observe that sol will change only when cnt is incremented; 
+            else{
+                hash[s[l]]--;
+                l++;
+            }
         }
-        
         return sol;
     }
 };
