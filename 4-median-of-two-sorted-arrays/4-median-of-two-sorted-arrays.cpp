0d03c1ad9292f5fafa503665ced481f44b2c0ed8
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-07-16 11:29:34 +0530

    Time: 58 ms (58.03%), Space: 89.3 MB (60.88%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,46 +1,33 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& a, vector<int>& b) {
+    double findMedianSortedArrays(vector<int> &a, vector<int> &b){
         int na=a.size(), nb=b.size();
-        if(nb<na) return findMedianSortedArrays(b, a); //cuz we want a to be smaller;
-        
-        
-        int total=na+nb; //size of final array;
+        if(nb<na) return findMedianSortedArrays(b, a);
+    
+        int total=na+nb;
         int half=(total+1)/2;
-        
-        //we are doing binary search on the smaller array;        
         int l=0, r=na;
-        
+    
         while(l<=r){
-            //i will pt to mid of a, and j will point to mid of b;
             int i=l+(r-l)/2;
-            int j=half-i; //cuz of 0-based indexing;
-            int aleft=(i-1<0)?INT_MIN:a[i-1];
-            int aright=(i>=na)?INT_MAX:a[i];
-            int bleft=(j-1<0)?INT_MIN:b[j-1];
-            int bright=(j>=nb)?INT_MAX:b[j];
+            int j=half-i;
             
-            //now we are checking if my partitions are correct;
+            int aleft=(i-1)>=0?a[i-1]: INT_MIN;
+            int aright=(i<na)?a[i]: INT_MAX;
+            int bleft=(j-1)>=0?b[j-1]: INT_MIN;
+            int bright=(j<nb)?b[j]: INT_MAX;
+    
             if(aleft<=bright && bleft<=aright){
-                if(total%2){ 
+                if(total%2){
                     return (double)max(aleft, bleft);
                 }
-                else{
-                    return (double) (max(aleft, bleft)+min(aright, bright))/2.0;
-                }
+                return (double)(max(aleft, bleft)+min(aright, bright))/2.0;
             }
-            else if(aleft>bright){
-                r=i-1;
-            }
-            else{
-                l=i+1;
-            }
+            else if(aleft>bright) r=i-1;
+            else l=i+1;
         }
-        
-        return 0;
+    
+        return 0.0;
     }
-};
 
-//in given case, a=[2]
-//b=[1,3]
-//half in odd case means median will be after those many elements;
+};
