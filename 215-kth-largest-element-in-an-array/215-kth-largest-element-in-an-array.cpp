7074bd1a30ff9349ecd984c486f7db2516b349d2
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-07-17 16:42:08 +0530

    Time: 293 ms (5.00%), Space: 45.4 MB (8.87%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -2,10 +2,28 @@ class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
         int n=nums.size();
-        nth_element(nums.begin(), nums.begin()+n-k, nums.end());
-        //first, sorted_position, last;
-        //so, sorted_position, will have the element that is in its correct position if the array is sorted;
-        //so other parts of the array is still unsorted, but this part is;
-        return nums[n-k];
+        int left=0, pivot=n-1, ind=n-k;
+      
+        while(left<=pivot){ 
+            int tmp=left;
+            for(int i=left; i<pivot; i++){ 
+                if(nums[i]<=nums[pivot]){
+                    swap(nums[i], nums[tmp]);
+                    tmp++;
+                }
+            }
+            swap(nums[pivot], nums[tmp]); //all the elements before tmp are smaller than or equal to pivot;
+            if(tmp==ind){
+                return nums[ind];
+            }
+            else if(tmp<ind){
+                left=tmp+1;
+            }
+            else{
+                pivot=tmp-1;
+            }
+        }
+        
+        return -1;
     }
 };
