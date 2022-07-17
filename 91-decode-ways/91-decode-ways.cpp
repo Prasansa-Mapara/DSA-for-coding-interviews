127ae9e4c27a1434c5ae309174c7ef8a24d3139f
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-07-17 22:50:48 +0530

    Time: 3 ms (60.67%), Space: 6.3 MB (46.49%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -3,18 +3,15 @@ public:
     int numDecodings(string s) {
         int n=s.size();
         vector<int> dp(n+1, 0);
-        //1,1,0
         dp[0]=1;
-        dp[1]=(s[0]=='0'?0:1);
-        for(int i=1; i<n; i++){
-            if(s[i]!='0'){
-                dp[i+1]=dp[i]; //calculating with only one letter;
+        dp[1]=s[0]=='0'?0:1; //if i take only 1 char, 1 way to decode it;
+        for(int i=2; i<=n; i++){
+            if(s[i-1]!='0') dp[i]+=dp[i-1];
+            int two_char=stoi(s.substr(i-2, 2));
+            if(two_char>=10 && two_char<=26){
+                dp[i]+=dp[i-2];
             }
-            int x=stoi(s.substr(i-1, 2));
-            if(x>=10 && x<=26){
-                dp[i+1]+=dp[i-1]; //calculating with second letter;
-            }
-        }
+        } 
         return dp[n];
     }
 };
