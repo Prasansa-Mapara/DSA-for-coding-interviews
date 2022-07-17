9eb56a3a29d9dafe254133652f64af0a9bf161cd
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-07-17 14:38:43 +0530

    Time: 15 ms (79.85%), Space: 21 MB (54.12%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/110-balanced-binary-tree.cpp
@@ -11,27 +11,22 @@
  */
 class Solution {
 public:
-    //we'll need heights to check if balanced;
-    //we need to check if each and every subtree is balanced;
-    //so while checking heights, if unbalanced, then we'll just return -1;
-    //as height can never be -1, this won't be an issue;
-    
-    int solve(TreeNode *curr){
-        if(!curr) return 0;
-        int l=solve(curr->left);
-        if(l==-1){ //if left subtree is unbalanced, then just return -1;
+    int findHeight(TreeNode *root){
+        if(!root) return 0;
+        int left=findHeight(root->left);
+        int right=findHeight(root->right);
+        if(left==-1 || right==-1) return -1;
+        if(abs(right-left)<=1){
+            return 1+max(left, right);
+        }
+        else{
             return -1;
         }
-        int r=solve(curr->right);
-        if(r==-1 || abs(r-l)>1){ //if right subtree is unbalanced or current is unbalanced;
-            return -1;
-        }
-        //everything's balanced, so return height;
-        return 1+max(l, r);
     }
     
     bool isBalanced(TreeNode* root) {
         if(!root) return 1;
-        return (solve(root)==-1)?0:1;
+        int currH=findHeight(root);
+        return currH==-1?0:1;
     }
 };
