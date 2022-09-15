986aeafa9c1f686bf9cdb91d28c4068d66a1849f
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-09-15 15:04:54 +0530

    Time: 86 ms (16.32%), Space: 12.6 MB (39.19%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,27 +1,35 @@
 class Solution {
 public:
-    void dfs(vector<vector<char>> &mat, int i, int j){
+    vector<vector<char>> mat;
+    
+    void dfs(int i, int j){
         int r=mat.size(), c=mat[0].size();
-        if(i<0 || j<0 || i>=r || j>=c || mat[i][j]=='0'){
+        if(i<0 || i>=r || j<0 || j>=c){
             return;
         }
-        mat[i][j]='0'; //marking it visited;
-        dfs(mat, i+1, j); 
-        dfs(mat, i-1, j);
-        dfs(mat, i, j+1);
-        dfs(mat, i, j-1);
+        if(mat[i][j]=='0'){
+            return;
+        }
+        mat[i][j]='0'; //marked as visited;
+        dfs(i+1, j);
+        dfs(i-1, j);
+        dfs(i, j+1);
+        dfs(i, j-1);
+        return;
     }
     
-    int numIslands(vector<vector<char>>& mat) {
-        int islands=0, r=mat.size(), c=mat[0].size();
+    int numIslands(vector<vector<char>>& grid) {
+        mat=grid;
+        int r=mat.size(), c=mat[0].size(), islands=0;
         for(int i=0; i<r; i++){
             for(int j=0; j<c; j++){
-                if(mat[i][j]=='1'){ //if its a land then run dfs;
-                    dfs(mat, i, j);
+                if(mat[i][j]=='1'){ //land
                     islands++;
-                }                
+                    dfs(i, j);
+                }
             }
         }
         return islands;
     }
 };
+
