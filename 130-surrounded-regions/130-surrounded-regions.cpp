08e8d95c0b0de0ab741216443f3a41627c4b4711
Prasansa Mapara <94972192+Prasansa-Mapara@users.noreply.github.com>
2022-09-15 16:19:08 +0530

    Time: 25 ms (37.74%), Space: 9.9 MB (76.89%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.cpp
+++ 130-surrounded-regions/130-surrounded-regions.cpp
@@ -1,40 +1,60 @@
 class Solution {
 public:
-    void dfs(vector<vector<char>> &mat, int i, int j){
-        int r=mat.size(), c=mat[0].size();
-        if(i<0 || j<0 || i>=r || j>=c || mat[i][j]!='O') return;
-        mat[i][j]='F'; //mark it visited;
-        dfs(mat, i+1, j); dfs(mat, i-1, j);
-        dfs(mat, i, j+1); dfs(mat, i, j-1);
+    
+    void dfs(int i, int j, vector<vector<char>> &mat)
+    {
+        int r = mat.size();
+        int c = mat[0].size();
+        
+        if(i<0 || i>=r || j<0 || j>=c || mat[i][j]=='X' || mat[i][j]=='p') 
+            return;
+        
+        
+        mat[i][j] = 'p';
+        
+        dfs(i,j+1,mat);
+        dfs(i+1,j,mat);
+        dfs(i-1,j,mat);
+        dfs(i,j-1,mat);
+        
+        return;
     }
     
-    void solve(vector<vector<char>>& mat) {
-        //basically we need corner vaale connected O's;
-        //then except for those, all will be converted;
-        int r=mat.size(), c=mat[0].size();
-        for(int i=0; i<r; i++){
-            if(mat[i][0]=='O'){
-                dfs(mat, i, 0);
-            }
-            if(mat[i][c-1]=='O'){
-                dfs(mat, i, c-1);
-            }
-        }
-        for(int j=0; j<c; j++){
-            if(mat[0][j]=='O'){
-                dfs(mat, 0, j);
-            }
-            if(mat[r-1][j]=='O'){
-                dfs(mat, r-1, j);
+    void solve(vector<vector<char>>& board) 
+    {
+        int r = board.size();
+        int c = board[0].size();
+        
+        for(int i=0;i<r;i++)
+        {
+            for(int j=0;j<c;j++)
+            {
+                if(i==0 || i== r-1 || j==0 || j==c-1)
+                 {
+                    if(board[i][j]=='O')
+                    dfs(i,j,board);
+                }
             }
         }
         
-        for(int i=0; i<r; i++){
-            for(int j=0; j<c; j++){
-                if(mat[i][j]=='O') mat[i][j]='X';
-                else if(mat[i][j]=='F') mat[i][j]='O';
+        for(int i=0;i<r;i++)
+        {
+            for(int j=0;j<c;j++)
+            {
+                if(board[i][j]=='O')
+                    board[i][j]='X';
             }
         }
+        
+        for(int i=0;i<r;i++)
+        {
+            for(int j=0;j<c;j++)
+            {
+                if(board[i][j]=='p')
+                    board[i][j]='O';
+            }
+        }
+        
         return;
     }
 };
